ef2a4d352e1596b86c6e414bb5f24e9238f8a53c
George Datseris <datseris.george@gmail.com>
2019-11-24 15:20:35 +0100

    add space-less Wealth for Literate.jl

---
+++ examples/wealth_distribution.jl
@@ -0,0 +1,58 @@
+# # Wealth distribution model
+
+# This model is a simple agent-based economy that is taken modelled according
+# to the work of [Dragulescu *et al.*](https://arxiv.org/abs/cond-mat/0211175).
+# This work introduces statistical mechanics concepts to study wealth distributions.
+# For this reason what we show here is also referred to as "Boltzmann wealth
+# distribution" model.
+
+# This model has a version with and without space.
+# The rules of the space-less game are quite simple:
+# 1. There is a pre-determined number of agents.
+# 2. All agents start with one unit of wealth.
+# 3. At every step an agent gives 1 unit of wealth (if they have it) to some other agent.
+
+# Even though these are some very simple rules, they can still create the basic
+# properties of wealth distributions, e.g. power-laws distributions.
+
+# ## Core structures, space-less
+# We start by defining the Agent type and initializing the model.
+using Agents
+mutable struct WealthyAgent <: AbstractAgent
+    id::Int
+    wealth::Int
+end
+
+# Notice that this agent does not have a `pos` field. That is okay, because
+# there is no space structure to this example.
+# We can also make a very simple [`AgentBasedModel`](@ref) for our model.
+# Because it is pre-determined how many agents the model we have, we can
+# even make it a parameter of the model for easy access.
+
+function wealth_model(numagents = 100)
+    p = (n = numagents,)
+    model = ABM(WealthyAgent, scheduler=random_activation, properties = p)
+
+    for i in 1:numagents
+        add_agent!(model, 1) # 1 is the initial wealth!
+    end
+    return model
+end
+
+model = wealth_model(100)
+
+# The next step is to define the agent step function
+function agent_step!(agent::AbstractAgent, model::ABM)
+    agent.wealth == 0 && return # do nothing
+    ragent = random_agent(model)
+    agent.wealth -= 1
+    random_agent.wealth += 1
+end
+
+# We use `random_agent` as a convenient way to just grab a random agent.
+# (this may return the same agent as `agent`, but we disregard it as noise)
+
+# ## Running the space-less model
+
+agent_properties = [:wealth]
+data = step!(wmodel, agent_step!, 10, agent_properties)
