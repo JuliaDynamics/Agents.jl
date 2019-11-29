d581f51122b964b06d52d8ad8b1bccc0ba05e108
Ali R. Vahdati <arv.kavir@gmail.com>
2019-11-30 00:56:59 +0100

    Add tests for when step0=false

--- test/data_collector_test.jl
+++ test/data_collector_test.jl
@@ -6,9 +6,23 @@ Random.seed!(223)
   when = 1:10
   data = step!(forest, dummystep, forest_step!, 10, agent_properties, when=when);
   @test size(data) == (11, 3)
+  
+  forest = model_initiation(f=0.05, d=0.8, p=0.01, griddims=(20, 20), seed=2);
+  agent_properties = Dict(:status => [length, count])
+  when = 1:10
+  data = step!(forest, dummystep, forest_step!, 10, agent_properties, when=when,
+  step0=false);
+  @test size(data) == (10, 3)
 
   agent_properties = [:status]
   forest = model_initiation(f=0.05, d=0.8, p=0.01, griddims=(20, 20), seed=2);
   data = step!(forest, dummystep, forest_step!, 10, agent_properties, when=when);
   @test size(data) == (993, 3)
+
+  forest = model_initiation(f=0.05, d=0.8, p=0.01, griddims=(20, 20), seed=2);
+  agent_properties = [:status]
+  when = 1:10
+  data = step!(forest, dummystep, forest_step!, 10, agent_properties, when=when,
+  step0=false);
+  @test size(data) == (666, 3)
 end
