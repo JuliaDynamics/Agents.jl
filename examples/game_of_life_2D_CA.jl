86e2e130bbc58aae843d37ba26fadf6218149255
Ali R. Vahdati <arv.kavir@gmail.com>
2019-12-01 13:47:44 +0100

    Update CA example comments for markdown

--- examples/game_of_life_2D_CA.jl
+++ examples/game_of_life_2D_CA.jl
@@ -1,30 +1,32 @@
+# # Two-dimensional cellular automata 
+# Agents.jl provides a module (CA2D) to create and plot 2D cellular automata.
+
 using Agents
 using Agents.CA2D
 using AgentsPlots
 
-# Agents.jl provides a module to create and plot 2D cellular automata.
-
-# Define the rule
+# ## 1. Define the rule
 # Rules of Conway's game of life: DSR (Death, Survival, Reproduction). 
 # Cells die if the number of their living neighbors are <D, 
 # survive if the number of their living neighbors are <=S,
 # come to life if their living neighbors are as many as R.
 rules = (2,3,3)
 
-# 1. Build the model
-# "CA2D.build_model" creates a model where all cells are "0"
+# ## 2. Build the model
+# "CA2D.build_model" creates a model where all cells are by default off ("0")
 model = CA2D.build_model(rules=rules, dims=(100, 100), Moore=true)
-# make some random cells alive
+
+# Let's make some random cells on
 for i in 1:nv(model)
   if rand() < 0.1
     model.agents[i].status="1"
   end
 end
 
-# 2. Run the model, collect data, and visualize it 
+# ## 3. Run the model and collect data
 runs = 10
 data = CA2D.ca_run(model, runs);
 
-# 3. Visualize the data
+# ## 4. Visualize the data
 # This creates one figure for each step of the simulation
 plot_CA2D(data, savename="gameOfLife", nodesize=2)
--- examples/rule22_1D_CA.jl
+++ examples/rule22_1D_CA.jl
@@ -1,22 +1,26 @@
+# # One-dimensional cellular automata 
+# Agents.jl provides a module (CA1D) to create and plot 1D cellular automata.
+
 using Agents
 using Agents.CA1D
 using AgentsPlots
 
-# Agents.jl provides a module to create and plot 1D cellular automata.
+# ## 1. Define the rule
+# Here is Wolfram's rule 22
 
-# 0. Define the rule
-# Wolfram's rule 22
 rules = Dict("111"=>"0", "110"=>"0", "101"=>"0", "100"=>"1", "011"=>"0",
             "010"=>"1", "001"=>"1", "000"=>"0")
 
-# 1. Build the model
+# ## 2. Build the model
+# All the cells are by default initially "off" 
 model = CA1D.build_model(rules=rules, ncols=101)
-# turn on the middle cell
+
+# This turns on the middle cell
 model.agents[51].status="1"
 
-# 2. Run the model and collect data.
+# ## 3. Run the model and collect data.
 runs = 100
 data = CA1D.ca_run(model, runs);
 
-# 3. Visualize the data
+# ## 4. Visualize the data
 plot_CA1D(data, savename="rule22", nodesize=2)
