df447b5c36c393c65220afe79e79ac0e9f904226
George Datseris <datseris.george@gmail.com>
2019-12-10 20:47:36 +0100

    add basic tests for schedulers

--- test/api_tests.jl
+++ test/api_tests.jl
@@ -20,3 +20,39 @@ pos1 = model1.space.agent_positions[coord2vertex((1,1), model1)]
 @test length(pos1) == 0
 pos2 = model1.space.agent_positions[coord2vertex((2,2), model1)]
 @test pos2[1] == 1
+
+# %% Scheduler tests
+N = 1000
+mutable struct Agent0 <: AbstractAgent
+  id::Int
+end
+# fastest
+model = ABM(Agent0)
+for i in 1:N; add_agent!(model); end
+@test sort!(collect(keys(model.agents))) == 1:N
+
+# by_id
+model = ABM(Agent0; scheduler = by_id)
+for i in 1:N; add_agent!(model); end
+
+@test sort!(collect(keys(model.agents))) == 1:N
+
+@test model.scheduler(model) == 1:N
+
+using Random
+Random.seed!(12)
+
+# random
+model = ABM(Agent0; scheduler = random_activation)
+for i in 1:N; add_agent!(model); end
+
+@test model.scheduler(model)[1:3] == [913, 522, 637] # reproducibility test
+
+# partial
+Random.seed!(12)
+model = ABM(Agent0; scheduler = partial_activation(0.1))
+for i in 1:N; add_agent!(model); end
+
+a = model.scheduler(model)
+@test length(a) < N
+@test a[1] == 74 # reproducibility test
