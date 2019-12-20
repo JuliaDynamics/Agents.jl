018c878039b39529c0ab769b84c70d886bc7d183
George Datseris <datseris.george@gmail.com>
2019-12-20 20:10:41 +0100

    Make Schelling a dedicated example and fix its errors (#116)
    * add schelling's example to a separate file
    
    * add model to the docs
    
    * fix missing ,

--- docs/make.jl
+++ docs/make.jl
@@ -13,7 +13,7 @@ CI && (ENV["GKSwstype"] = "100")
 # %% Literate convertion
 indir = joinpath(@__DIR__, "..", "examples")
 outdir = joinpath(@__DIR__, "src", "examples")
-for file in ("forest_fire.jl", "wealth_distribution.jl",
+for file in ("schelling.jl", "forest_fire.jl", "wealth_distribution.jl",
 			 "rule22_1D_CA.jl", "game_of_life_2D_CA.jl", "wright-fisher.jl")
 	Literate.markdown(joinpath(indir, file), outdir)
 end
@@ -31,6 +31,7 @@ pages = [
 	"Tutorial" => "tutorial.md",
 	"API" => "api.md",
 	"Examples" => [
+		"Schelling's segregation model" => "examples/schelling.md",
 		"Wealth distribution" => "examples/wealth_distribution.md",
 		"Forest fire" => "examples/forest_fire.md",
 		"Game of life" => "examples/game_of_life_2D_CA.md",
