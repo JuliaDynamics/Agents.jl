a6b7decff470543a6a0292919a24a59839684330
Datseris <datseris.george@gmail.com>
2019-12-24 13:45:32 +0100

    Add HK model to Literate convertion

--- examples/HK.jl
+++ examples/HK.jl
@@ -1,24 +1,26 @@
 # # HK (Hegselmann and Krause) opinion dynamics model
 
 # This is an implementation of a simple version of the Hegselman and Krause
-# [Hegselmann and Krause (2002)](http://jasss.soc.surrey.ac.uk/5/3/2.html) model.
+# [Hegselmann and Krause (2002)](http://jasss.soc.surrey.ac.uk/5/3/2.html) model,
+# which also features synchronous updating of Agent properties.
+
 # It is a model of opinion formation with the question: which
 # parameters' values lead to consensus, polarization or fragmentation?
-
 # It models interacting **groups** of agents (as opposed to interacting pairs, typical in
 # the literature) in which it is assumed that if an agent disagrees too much with
 # the opinion of a source of influence, the source can no longer influence the
 # agent’s opinion. There is then a "bound of confidence". The model shows that the
 # systemic configuration is heavily dependent on this parameter's value.
+
 # We implement it as an example of how to implement a [Synchronous update schedule](http://jmckalex.org/compass/syn-and-asynch-expl.html) .
-# In a Synchronous update schedule changes made to an agent are not seen by 
-# other agents until the next clock tick — that is, 
+# In a Synchronous update schedule changes made to an agent are not seen by
+# other agents until the next clock tick — that is,
 # all agents update simultaneously [Wilensky 2015, p.286](https://mitpress.mit.edu/books/introduction-agent-based-modeling)
 
 
 # The model has the following components:
 
-# - A set of n Agents with opinions xᵢ in the range [0,1] as attribute; 
+# - A set of n Agents with opinions xᵢ in the range [0,1] as attribute;
 # - A bound ϵ in also in the range [0,1] (actually, the range of interesting results is
 # approximately (0, 0.3]);
 # - The update rule: at each step every agent adopts the mean of the opinions which are within
@@ -29,7 +31,7 @@
 # We start by defining the Agent type and initializing the model.
 # The Agent type has two fields so that we can implement the synchronous update.
 using Agents
-using Distributions: mean 
+using Distributions: mean
 
 
 mutable struct HKAgent{T <: AbstractFloat} <: AbstractAgent
@@ -54,12 +56,12 @@ end
 # is applied to the  `:old_opinion` field .
 get_old_opinion(agent)::Float64 = agent.old_opinion
 
-function boundfilter(agent,model) 
+function boundfilter(agent,model)
     filter(j->abs(get_old_opinion(agent) - j) < model.properties[:ϵ],
      get_old_opinion.(values(model.agents)))
 end
 
-# Now we implement the `agent_step!` and `model_step!` methods. 
+# Now we implement the `agent_step!` and `model_step!` methods.
 function agent_step!(agent, model)
     agent.new_opinion = mean(boundfilter(agent,model))
 end
@@ -71,15 +73,15 @@ function model_step!(model)
     end
 end
 
-# From this implementation we see that to implement synchronous scheduling 
-# we define an Agent type with an `old` and `new` fields for attributes that 
+# From this implementation we see that to implement synchronous scheduling
+# we define an Agent type with an `old` and `new` fields for attributes that
 # are changed through synchronous updating. In the agent_step! we use the `old` field
 # and after updating all the agents `new` field we use the `model_step!`
 # to update the model  for the next iteration.
 
-# Now we can define a method for our simulation run. 
+# Now we can define a method for our simulation run.
 # The parameter of interest is the :new_opinion` field so we assign
-# it to variable agent_properties and pass it to the `step!` method 
+# it to variable agent_properties and pass it to the `step!` method
 # to be collected in a DataFrame.
 function model_run(; numagents = 100, iterations = 50, ϵ= 0.3)
     model = hk_model(numagents = numagents, ϵ = ϵ)
@@ -87,12 +89,12 @@ function model_run(; numagents = 100, iterations = 50, ϵ= 0.3)
     agent_properties = [:new_opinion]
     data = step!(
             model,
-            agent_step!, 
+            agent_step!,
             model_step!,
-            iterations, 
+            iterations,
             agent_properties,
             when = when
-            ) 
+            )
     return(data)
 end
 
@@ -108,5 +110,3 @@ plt001,plt015,plt03 = map(e -> (model_run(ϵ= e), e) |>
 t -> plotsim(t[1], t[2]), [0.05, 0.15, 0.3])
 
 foreach(display, (plt001,plt015,plt03))
-
-
