ef8cde9449363ef1b952512b9ba00f5b13716a01
Marcelo Maciel <marcelovmaciel@users.noreply.github.com>
2019-12-24 10:10:25 -0300

    Update examples/HK.jl
    Co-Authored-By: Susannah Klaneček <suzil@users.noreply.github.com>

--- examples/HK.jl
+++ examples/HK.jl
@@ -53,7 +53,7 @@ end
 
 # And some helper functions for the update rule. As there is a filter in
 # the rule we implement it outside the `agent_step!` method. Notice that the filter
-# is applied to the  `:old_opinion` field .
+# is applied to the `:old_opinion` field.
 get_old_opinion(agent)::Float64 = agent.old_opinion
 
 function boundfilter(agent,model)
