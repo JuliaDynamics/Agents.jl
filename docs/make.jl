eee240087145c0f72091c3e8d1343243617a6b05
Datseris <datseris.george@gmail.com>
2020-03-12 17:07:52 +0100

    use JuliaDynamics theme

--- docs/make.jl
+++ docs/make.jl
@@ -20,6 +20,22 @@ for file in ("schelling.jl", "forest_fire.jl", "wealth_distribution.jl",
 end
 
 # %%
+# download the themes
+using DocumenterTools: Themes
+for file in ("juliadynamics-lightdefs.scss", "juliadynamics-darkdefs.scss", "juliadynamics-style.scss")
+    download("https://raw.githubusercontent.com/JuliaDynamics/doctheme/master/$file", joinpath(@__DIR__, file))
+end
+# create the themes
+for w in ("light", "dark")
+    header = read(joinpath(@__DIR__, "juliadynamics-style.scss"), String)
+    theme = read(joinpath(@__DIR__, "juliadynamics-$(w)defs.scss"), String)
+    write(joinpath(@__DIR__, "juliadynamics-$(w).scss"), header*"\n"*theme)
+end
+# compile the themes
+Themes.compile(joinpath(@__DIR__, "juliadynamics-light.scss"), joinpath(@__DIR__, "src/assets/themes/documenter-light.css"))
+Themes.compile(joinpath(@__DIR__, "juliadynamics-dark.scss"), joinpath(@__DIR__, "src/assets/themes/documenter-dark.css"))
+
+# %%
 cd(@__DIR__)
 makedocs(modules = [Agents,AgentsPlots],
 sitename= "Agents.jl",
