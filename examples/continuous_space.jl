a52cb393d1dbd4bd384764afa6f686c1f12e32db
Ali R. Vahdati <arv.kavir@gmail.com>
2020-03-17 11:47:19 +0100

    Restructure types

--- examples/continuous_space.jl
+++ examples/continuous_space.jl
@@ -9,22 +9,20 @@ import Agents: Space
 import Base.show
 import Agents: add_agent!
 
-mutable struct Agent <: AbstractAgent
-  id::Int
-  pos::Tuple{Float64, Float64}
-  dir::Float64  # direction of movement
+mutable struct Agent{T<:Int,F<:AbstractFloat} <: AbstractAgent
+  id::T
+  pos::Tuple{F, F}
+  vel::Tuple{F, F}  # velocity: speed and direction
+  diameter::F
 end
 
 struct ContinuousSpace <: AbstractSpace  
   db::SQLite.DB
-  movesize
-  resolution
-  interaction_radius
-  insertq
+  insertq::SQLite.Stmt
 end
 
 "Initializes a database with an empty table."
-function ContinuousSpace(;movesize=0.005, resolution=3, interaction_radius=10.0^(-resolution))
+function ContinuousSpace()
   db = SQLite.DB()
   stmt = "CREATE TABLE tab (
     x REAL,
@@ -33,16 +31,14 @@ function ContinuousSpace(;movesize=0.005, resolution=3, interaction_radius=10.0^
   DBInterface.execute(db, stmt)
   insertstmt = "INSERT INTO tab (x, y, id) VALUES (?, ?, ?)"
   q = DBInterface.prepare(db, insertstmt)
-  ContinuousSpace(db, movesize, resolution, interaction_radius, q)
+  ContinuousSpace(db, q)
 end
-
-Space(;movesize, resolution, interaction_radius=10.0^(-resolution)) = ContinuousSpace(movesize=movesize,resolution=resolution,interaction_radius=interaction_radius)
 
 "Add many agents to the database"
 function fill_db!(agents, model::ABM{A, S}) where {A, S<:ContinuousSpace}
   db = model.space.db
   for agent in agents
-    p1, p2 = round.(agent.pos, digits=model.space.resolution)
+    p1, p2 = agent.pos
     DBInterface.execute(model.space.insertq, [p1, p2, agent.id])
   end
 end
@@ -54,53 +50,53 @@ data can become slower because after each addition, index needs to be reworked.
 Lack of index won't be noticed for small databases. Only use it when you have 
 many agents and not many additions of agents.
 """
-function index_db!(model)
+function index!(model)
   DBInterface.execute(model.space.db, "CREATE INDEX pos ON tab (x,y,id)")
 end
 
 # TODO
 function Base.show(io::IO, abm::ContinuousSpace)
-    # s = "$(nameof(typeof(abm))) with $(nv(abm)) nodes and $(ne(abm)) edges"
     s = "A ContinuousSpace"
     print(io, s)
 end
 
 function add_agent!(model::ABM{A, S}, properties...) where {A, S<:ContinuousSpace}
   db = model.space.db
-  
   idq = DBInterface.execute(db, "select max(id) as maxid from tab") |> DataFrame
   id = ismissing(idq[1,1]) ? 1 : idq[1,1]+1
   agent = A(id, properties...)
-  p1, p2 = round.(agent.pos, digits=model.space.resolution)
+  p1, p2 = agent.pos
   DBInterface.execute(model.space.insertq, [p1, p2, id])
   model.agents[id] = agent
   return agent
 end
 
-function model_initiation(;N=100, movesize=0.005, space_resolution=3, seed=0)
+function model_initiation(;N=100, speed=0.005, space_resolution=0.001, seed=0)
   Random.seed!(seed)
-  space = Space(movesize=movesize, resolution=space_resolution)
+  space = ContinuousSpace()
   model = ABM(Agent, space);  # TODO fix Base.show
   
   ## Add initial individuals
   for ind in 1:N
-    pos = Tuple(round.(rand(2), digits=space_resolution))
-    add_agent!(model, pos, rand(0:0.01:2π))
+    pos = Tuple(rand(0.0:space_resolution:1.0, 2))
+    vel = (speed, rand(0:0.01:2π))
+    dia = space_resolution * 3
+    add_agent!(model, pos, vel, dia)
   end
  
-  index_db!(model)
+  index!(model)
 
   return model
 end
 
 function agent_step!(agent, model)
-  move!(agent, model)
+  move!(agent)
   collide!(agent, model)
 end
 
-function move!(agent, model)
-  newx = agent.pos[1] + (model.space.movesize * cos(agent.dir))
-  newy = agent.pos[2] + (model.space.movesize * sin(agent.dir))
+function move!(agent)
+  newx = agent.pos[1] + (agent.vel[1] * cos(agent.vel[2]))
+  newy = agent.pos[2] + (agent.vel[1] * sin(agent.vel[2]))
   newx < 0.0 && (newx = newx + 1.0)
   newx > 1.0 && (newx = newx - 1.0)
   newy < 0.0 && (newy = newy + 1.0)
@@ -110,7 +106,7 @@ end
 
 function collide!(agent, model)
   db = model.space.db
-  interaction_radius = model.space.interaction_radius
+  interaction_radius = agent.diameter
   xleft = agent.pos[1] - interaction_radius
   xright = agent.pos[1] + interaction_radius
   yleft = agent.pos[2] - interaction_radius
@@ -120,14 +116,14 @@ function collide!(agent, model)
   size(r,1) == 0 && return
   # change direction
   firstcontact = id2agent(r[1,:id], model)
-  agent.dir, firstcontact.dir = firstcontact.dir, agent.dir
+  agent.vel, firstcontact.vel = (agent.vel[1], firstcontact.vel[2]), (firstcontact.vel[1], agent.vel[2])
 end
 
-model = model_initiation(N=100, movesize=0.005)
+model = model_initiation(N=100, speed=0.005, space_resolution=0.001);
 step!(model, agent_step!, 500)
 
 # ## Example animation
-model = model_initiation(N=100, movesize=0.005)
+model = model_initiation(N=100, speed=0.005, space_resolution=0.001)
 anim = @animate for i ∈ 1:100
   xs = [a.pos[1] for a in values(model.agents)];
   ys = [a.pos[2] for a in values(model.agents)];
