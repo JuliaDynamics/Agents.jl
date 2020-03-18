d30f6874b78bd5da51b9baa690551421cac4d4c9
Datseris <datseris.george@gmail.com>
2020-03-18 01:33:49 +0100

    make ContinuousSpace dimension generic

--- examples/continuous_space.jl
+++ examples/continuous_space.jl
@@ -9,40 +9,63 @@ import Agents: Space
 import Base.show
 import Agents: add_agent!
 
-mutable struct Agent{T<:Int,F<:AbstractFloat} <: AbstractAgent
-  id::T
-  pos::Tuple{F, F}
-  vel::Tuple{F, F}  # velocity: speed and direction
+const COORDS = collect(Iterators.flatten(('x':'z', 'a':'w')))
+
+mutable struct Agent{D, F<:AbstractFloat} <: AbstractAgent
+  id::Int
+  pos::NTuple{D, F}
+  vel::NTuple{D, F}  # velocity: speed and direction
   diameter::F
 end
 
-struct ContinuousSpace <: AbstractSpace  
+struct ContinuousSpace{E} <: AbstractSpace
+  D::Int
+  periodic::Bool
+  extend::E
   db::SQLite.DB
   insertq::SQLite.Stmt
   searchq::SQLite.Stmt
 end
 
-"Initializes a database with an empty table."
-function ContinuousSpace()
+"""
+    Space(D::Int [, vel!]; periodic::Bool = false, extend = nothing)
+Create a *continuous* space of dimensionality `D`.
+In this case, your agent positions (field `pos`) should be of type `NTuple{D, F}`
+where `F <: AbstractFloat`.
+In addition, the agent type must have a third field `vel::NTuple{D, F}` representing
+the agent's velocity.
+
+The optional argument `vel` is a **function**, `vel!(agent, model)` that updates
+the agent's velocities **before** the agent has been moved, see [`move_agent!`](@ref).
+By default no update is done this way (you can of course change the agents velocities
+during the agent interaction, the `vel!` functionality targets arbitrary forces).
+
+# TODO: talk about periodicity
+
+## Notes
+You can imagine the evolution algorithm as an Euler scheme with `dt = 1` (here the step).
+"""
+function Space(D::Int = 2, vel = nothing; periodic = false, extend = nothing)
+  # TODO: actually implement periodicity
   db = SQLite.DB()
-  stmt = "CREATE TABLE tab (
-    x REAL,
-    y REAL,
-    id INTEGER PRIMARY KEY)"
+  dimexpression = join("$x REAL, " for x in COORDS[1:D])
+  stmt = "CREATE TABLE tab ("*dimexpression*"id INTEGER PRIMARY KEY)"
   DBInterface.execute(db, stmt)
-  insertstmt = "INSERT INTO tab (x, y, id) VALUES (?, ?, ?)"
+  insertedxpression = join("$x, " for x in COORDS[1:D])
+  qmarks = join("?, " for _ in 1:D)
+  insertstmt = "INSERT INTO tab ($(insertedxpression)id) VALUES ($(qmarks)?)"
   q = DBInterface.prepare(db, insertstmt)
-  searchq = "SELECT id FROM tab WHERE x BETWEEN ? AND ? AND y BETWEEN ? AND ? AND id != ?"
+  searchexpr = join("$x BETWEEN ? AND ? AND " for x in COORDS[1:D])
+  searchq = "SELECT id FROM tab WHERE $(searchexpr)id != ?"
   q2 = DBInterface.prepare(db, searchq)
-  ContinuousSpace(db, q, q2)
+  ContinuousSpace(D, periodic, extend, db, q, q2)
 end
 
 "Add many agents to the database"
 function fill_db!(agents, model::ABM{A, S}) where {A, S<:ContinuousSpace}
   db = model.space.db
   for agent in agents
-    p1, p2 = agent.pos
-    DBInterface.execute(model.space.insertq, (p1, p2, agent.id))
+    DBInterface.execute(model.space.insertq, (agent.pos..., agent.id))
   end
 end
 
@@ -59,26 +82,31 @@ end
 Indexing the database can drastically improve retrieving data, but adding new
 data can become slower because after each addition, index needs to be reworked.
 
-Lack of index won't be noticed for small databases. Only use it when you have 
+Lack of index won't be noticed for small databases. Only use it when you have
 many agents and not many additions of agents.
 """
 function index!(model)
-  DBInterface.execute(model.space.db, "CREATE INDEX pos ON tab (x,y,id)")
+  D = model.space.D
+  expr = join("$x," for x in COORDS[1:D])
+  DBInterface.execute(model.space.db, "CREATE INDEX pos ON tab ($(expr)id)")
 end
 
-# TODO
+# TODO: re-check this at the end.
 function Base.show(io::IO, abm::ContinuousSpace)
-    s = "A ContinuousSpace"
+    s = "$(abm.D)-dimensional $(abm.periodic ? "periodic " : "")ContinuousSpace"
     print(io, s)
 end
 
+# TODO: This should also have a version with random position
 function add_agent!(model::ABM{A, S}, properties...) where {A, S<:ContinuousSpace}
   db = model.space.db
+
+  # TODO: This seems ineficient... Is there no way to directly get maximum of
+  # the column "id" of the database? There _has_ to be a way for it.
   ids = collect_ids(DBInterface.execute(db, "select max(id) as id from tab"))
   id = ismissing(ids[1]) ? 1 : ids[1]+1
   agent = A(id, properties...)
-  p1, p2 = agent.pos
-  DBInterface.execute(model.space.insertq, (p1, p2, id))
+  DBInterface.execute(model.space.insertq, (agent.pos..., id))
   model.agents[id] = agent
   return agent
 end
@@ -87,7 +115,7 @@ function model_initiation(;N=100, speed=0.005, space_resolution=0.001, seed=0)
   Random.seed!(seed)
   space = ContinuousSpace()
   model = ABM(Agent, space);  # TODO fix Base.show
-  
+
   ## Add initial individuals
   for ind in 1:N
     pos = Tuple(rand(0.0:space_resolution:1.0, 2))
@@ -95,7 +123,7 @@ function model_initiation(;N=100, speed=0.005, space_resolution=0.001, seed=0)
     dia = space_resolution * 10
     add_agent!(model, pos, vel, dia)
   end
- 
+
   index!(model)
 
   return model
@@ -105,6 +133,9 @@ function agent_step!(agent, model)
   move!(agent)
   collide!(agent, model)
 end
+
+# TODO: change move! to instead be an overload of move_agent!
+# and to call `vel!` first on the agent.
 
 function move!(agent)
   newx = agent.pos[1] + (agent.vel[1] * cos(agent.vel[2]))
@@ -145,5 +176,3 @@ end
 gif(anim, "movement.gif", fps = 8);
 
 # ![](social_distancing.gif)
-
-
