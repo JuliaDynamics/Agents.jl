b089993a152ae15f290d07518ceddbb559e9143e
Datseris <datseris.george@gmail.com>
2020-04-05 00:19:22 +0200

    remove operations from benchmark
    We should keep track of time for operations like modulo, etc.
    
    I also added two differently typed fiels to the agent, for further testing impact of type stability

--- test/benchmark/definitions.jl
+++ test/benchmark/definitions.jl
@@ -6,28 +6,20 @@ using StatsBase: mean
 mutable struct Agent1 <: AbstractAgent
   id::Int
   weight::Float64
+  type::String
 end
 function init1()
     Random.seed!(267)
     model = ABM(Agent1;
                 properties = Dict(:year => 0, :tick => 0, :flag => false))
     for a in 1:1000
-        add_agent!(model, rand())
+        add_agent!(model, rand(), randstring(4))
     end
     return model
 end
 function as1!(agent, model)
-    if rand() < 0.1
-        agent.weight += 0.05
-    end
-    if model.tick%365 == 0
-        agent.weight *= 2
-    end
+    nothing
 end
 function ms1!(model)
-    model.tick += 1
-    model.flag = rand(Bool)
-    if model.tick%365 == 0
-        model.year += 1
-    end
+    nothing
 end
