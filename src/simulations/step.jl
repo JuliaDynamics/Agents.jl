3449d9ad5e8480ddbe0b09779afc8a2467668ee1
Datseris <datseris.george@gmail.com>
2020-07-20 18:25:17 +0200

    bugfix step! method with no model

--- src/simulations/step.jl
+++ src/simulations/step.jl
@@ -7,11 +7,12 @@ Update agents `n` steps. Agents will be updated as specified by the `model.sched
 If given the optional function `model_step!`, it is triggered _after_ every scheduled
 agent has acted.
 
-    step!(model, agent_step! [, model_step!], n::Function)
+    step!(model, agent_step!, model_step!, n::Function)
 
 `n` can be also be a function.
 Then `step!` runs the model until `n(model, s)` returns `true`, where `s` is the
 current amount of steps taken (starting from 0).
+(in this case `model_step!` must be provided always, see [`dummystep`](@ref))
 """
 function step! end
 
@@ -31,7 +32,7 @@ dummystep(agent, model) = nothing
 until(ss, n::Int, model) = ss < n
 until(ss, n, model) = !n(model, ss)
 
-step!(model::ABM, agent_step!, n = 1) = step!(model, agent_step!, dummystep, n)
+step!(model::ABM, agent_step!, n::Int=1) = step!(model, agent_step!, dummystep, n)
 
 function step!(model::ABM, agent_step!, model_step!, n)
   s = 0
