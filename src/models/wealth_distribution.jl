0ba072df6d38881a3e708579afd35a552c596611
Frederik Banning <frederik.banning@ruhr-uni-bochum.de>
2020-07-31 15:20:08 +0200

    Clean up docstring

--- src/models/wealth_distribution.jl
+++ src/models/wealth_distribution.jl
@@ -3,16 +3,19 @@ mutable struct WealthAgent <: AbstractAgent
     wealth::Int
 end
 
-"""
-    wealth_distribution(; numagents = 100, initwealth = 1)
-Same as in [Wealth Distribution model](@ref).
+""" julia
+wealth_distribution(; 
+    numagents = 100, 
+    initwealth = 1
+)
+Same as in [Wealth distribution model](@ref).
 """
 function wealth_distribution(; numagents = 100, initwealth = 1)
     model = ABM(WealthAgent, scheduler = random_activation)
     for i in 1:numagents
         add_agent!(model, initwealth)
     end
-    return model, agent_step!, dummystep
+    return model, agent_step!
 end
 
 function agent_step!(agent, model)
