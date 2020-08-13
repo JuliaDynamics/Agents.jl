7b05f7df6c41a59b805c355fa68cd2ccdac5f252
Tim DuBois <tim@neophilus.net>
2020-08-13 23:39:46 +0200

    Text rewrite

--- examples/optim.jl
+++ examples/optim.jl
@@ -1,17 +1,19 @@
 # # Optimizing agent-based models
 
-# Agent-based models (ABMs) are computationally more expensive than analytical models, and can have many parameters. Sometimes we need to fine-tune a model's parameters to a specific outcome. Brute-force algorithms can take too long for testing each parameter setting. Even if it was feasbile to run the model for every parameter setting, it would not be enough because ABMs are stochastic and the effect of a parameter setting should be derived from running the model several times and taking its average behavior.
+# Sometimes we need to fine-tune our ABMs parameters to a specific outcome. The brute-force solution can quickly become infeasible for even for a few different parameter settings over a number of valid scan ranges. Most of the time, ABMs are also stochastic, so the effect of a parameter setting should be derived from taking the average value only after running the model several times.
 
-# Here we show how to use the evolutionary algorithms in [BlackBoxOptim.jl](https://github.com/robertfeldt/BlackBoxOptim.jl) with Agents.jl, to optimize the parameters of an epidemiological model (SIR). We explain this model in detail in [SIR model for the spread of COVID-19](@ref). For brevity here, we just import it.
+# Here we show how to use the evolutionary algorithms in [BlackBoxOptim.jl](https://github.com/robertfeldt/BlackBoxOptim.jl) with Agents.jl, to optimize the parameters of an epidemiological model (SIR). We explain this model in detail in [SIR model for the spread of COVID-19](@ref). For brevity here, we just import
 
 # ```julia
 # include("siroptim.jl") # From the examples directory
 # ```
 
+# which provides us a `model_initiation` helper function to build a SIR model, and an `agent_step!` function.
+
 include(joinpath(@__DIR__, "../../../examples/siroptim.jl")) #hide
 nothing #hide
 
-# Now we need to define a cost function. The cost function takes as agruments the model parameters that we want to tune, here migration rate, death rate, transmission rate when an infected person has been (not) detected (`β_det`, `β_und`), infection period, reinfection probability, and time until the infection is detected. The function returns one or more numbers as the objective to be minimized. Here, we try to minimize the number of infected people after 50 days.
+# To look for optimal parameters, we need to define a cost function. The cost function takes as arguments the model parameters that we want to tune; in a SIR model, that would be the migration rate, death rate, transmission rate, when an infected person has been detected (`β_det`), or when the remain undetected (`β_und`), infection period, reinfection probability, and time until the infection is detected. The function returns an *objective*: this value takes the form one or more numbers, which the optimiser will attempt to minimize.
 
 using BlackBoxOptim, Random
 using Statistics: mean
@@ -43,26 +45,27 @@ function cost(x)
     return mean(data.infected_fraction)
 end
 
-# Because ABMs are stochastic, we run 10 replicates and take the average fraction of infected people after 50 days
+# This cost function runs our model 10 times for 50 days, then returns the average number of infected people.
+# When we pass this function to an optimiser, we will effectively be asking for a set of parameters that can reduce the number of infected people to the lowest possible number.
 
 # We can now test the function cost with some reasonable parameter values.
 
 Random.seed!(10)
 
 x0 = [
-    0.2,  # migration_rate = 0.2,
-    0.1,  # death_rate = 0.1,
-    0.05, # β_det = 0.05,
-    0.3,  # β_und = 0.3,
-    10,   # infection_period = 10,
-    0.1,  # reinfection_probability = 0.1,
-    5,    # detection_time = 5,
+    0.2,  # migration_rate
+    0.1,  # death_rate
+    0.05, # β_det
+    0.3,  # β_und
+    10,   # infection_period
+    0.1,  # reinfection_probability
+    5,    # detection_time
 ]
 cost(x0)
 
-# After 50 days, 94% of the population is infected.
+# With these initial values, 94% of the population is infected after the 50 day period.
 
-# We let the optimization algorithm change parameters to minimize the number of infected individuals. Note that we can limit the allowed range for each parameter separately.
+# We now let the optimization algorithm change parameters to minimize the number of infected individuals. Complete details on how to use this optimiser can be found in the [BlackBoxOptim readme](https://github.com/robertfeldt/BlackBoxOptim.jl). Here, we assign a range of possible parameter values we would like to test, and a cutoff time in the event that certain parameter sets are unfeasible and cause our model to never converge to a solution.
 
 result = bboptimize(
     cost,
@@ -80,13 +83,18 @@ result = bboptimize(
 )
 best_fitness(result)
 
-# The fraction of the infected is down to 11%. Parameter values that give this result are:
+# With the new parameter values found in `result`, we find that the fraction of the infected population can be dropped down to 11%.
+# These values of these parameters are now:
 
 best_candidate(result)
 
-# We notice that the death rate is 96%, and transmission rates have also increased, while reinfection probability is much smaller. When all the infected indiduals die, infection doesn't transmit. Let's modify the cost function to also keep the mortality rate low.
+# Unfortunately we've not given the optimiser information we probably needed to. Notice that the death rate is 96%, with reinfection quite low.
+# When all the infected individuals die, infection doesn't transmit - the optimiser has managed to reduce the infection rate by killing the infected.
 
-# This can be tested by running the model with the new parameter values:
+# This is not the work of some sadistic AI, just an oversight in our instructions.
+# Let's modify the cost function to also keep the mortality rate low.
+
+# First, we'll run the model with our new-found parameters:
 
 x = best_candidate(result)
 
@@ -108,9 +116,9 @@ _, data =
 
 mean(data.nagents)
 
-# About 10% of the population dies with these parameters.
+# About 10% of the population dies with these parameters over our 50 day window.
 
-# We can define a multi-objective cost function that minimizes the number of infected and deaths.
+# We can define a multi-objective cost function that minimizes the number of infected and deaths by returning more than one value in our cost function.
 
 function cost_multi(x)
     model = model_initiation(;
@@ -142,11 +150,13 @@ function cost_multi(x)
     return mean(data.infected_fraction), mean(data.n_fraction)
 end
 
-# The cost of our initial parameter values is high: most of the population (96%) is infected and 22% die.
+# Notice that our new objective `n_fraction` is negative. It would be simpler to state we'd like to 'maximise the living population', but the optimiser we're using here focuses on minimising objectives only, therefore we must 'minimise the number of agents dying'.
 
 cost_multi(x0)
 
-# Let's minimize this multi-objective cost function. We need to define the optimization method for multi-objective functions:
+# The cost of our initial parameter values is high: most of the population (96%) is infected and 22% die.
+
+# Let's minimize this multi-objective cost function. There is more than one way to approach such an optimisation. Again, refer to the [BlackBoxOptim readme](https://github.com/robertfeldt/BlackBoxOptim.jl) for specifics.
 
 result = bboptimize(
     cost_multi,
@@ -164,22 +174,23 @@ result = bboptimize(
     NumDimensions = 7,
     MaxTime = 55,
 )
-
-# With the optimized parameters, about 0.3% of the population dies and 0.02% are infected:
-
 best_fitness(result)
 
-# And the tuned parameters are
+#
 
 best_candidate(result)
 
-# The algorithm managed to minimize the the number of infected and deaths while still increasing death rate to 42%, reinfection probability to 53%, and migration rates to 33%. The most important change decreasing the transmission rate when individuals are infected and undetected (from 30% to 0.2%).
+# These parameters look better: about 0.3% of the population dies and 0.02% are infected:
 
-# Let's reduce death rate and check the cost:
+# The algorithm managed to minimize the number of infected and deaths while still increasing death rate to 42%, reinfection probability to 53%, and migration rates to 33%.
+# The most important change however, was decreasing the transmission rate when individuals are infected and undetected from 30% in our initial calculation, to 0.2%.
+
+# Over a longer period of time than 50 days, that high death rate will take its toll though. Let's reduce that rate and check the cost.
 
 x = best_candidate(result)
 x[2] = 0.02
 cost_multi(x)
 
-# The fraction of infected increases to 0.04%. This is an interesting result, confirming the importance of social distancing. Without changing infection period and travel rate, even by increasing the transmission rate of the infected and detected (from 5% to 20%), by just decreasing the transmission rate of the undetected individuals, death rate drops 73 times and the number of infected decreases from 96% of the population to 3%.
+# The fraction of infected increases to 0.04%. This is an interesting result: since this virus model is not as deadly, the chances of re-infection increase.
+# We now have a set of parameters to strive towards in the real world. Insights such as these assist us to enact countermeasures like social distancing to mitigate infection risks.
 
