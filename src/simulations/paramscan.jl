e37d25cdb68685fb42892e22ed24a20beadeb8ca
Ali Rezaee <arv.kavir@gmail.com>
2020-09-04 17:12:20 +0200

    Remove agents_first from paramscan

--- src/simulations/paramscan.jl
+++ src/simulations/paramscan.jl
@@ -19,8 +19,7 @@ can take both model and agent constructor properties.
 All the following keywords are propagated into [`run!`](@ref).
 Defaults are also listed for convenience:
 `agent_step! = dummystep, n = 1, when = 1:n, model_step! = dummystep`,
-`step0::Bool = true`, `parallel::Bool = false`, `replicates::Int = 0`,
-agents_first::Bool=true.
+`step0::Bool = true`, `parallel::Bool = false`, `replicates::Int = 0`.
 Keyword arguments such as `adata` and `mdata` are
 also propagated.
 
@@ -34,7 +33,7 @@ included in the output `DataFrame`.
 function paramscan(parameters::Dict{Symbol,}, initialize;
   n = 1, agent_step! = dummystep,  model_step! = dummystep,
   progress::Bool = true, include_constants::Bool = false,
-  agents_first::Bool=true, kwargs...)
+  kwargs...)
 
   if include_constants
     changing_params = collect(keys(parameters))
