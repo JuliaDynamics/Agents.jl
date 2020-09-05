8e5d3e4035259d4fbe42d90d423ef06088e7c6c3
Tim DuBois <tim@neophilus.net>
2020-09-05 21:26:29 +0200

    Attempt to fix docs output

--- examples/sugarscape.jl
+++ examples/sugarscape.jl
@@ -10,7 +10,7 @@
 
 # ## Model structure
 
-# The first model that Epstein & Axtell (1996) present comprises a finite population of agents who live in an environment. The environment is represented by a two-dimensional grid which contains sugar in some of its cells, hence the name Sugarscape. Agents' role in this first model consists in wandering around the Sugarscape harvesting the greatest amount of sugar they can find. 
+# The first model that Epstein & Axtell (1996) present comprises a finite population of agents who live in an environment. The environment is represented by a two-dimensional grid which contains sugar in some of its cells, hence the name Sugarscape. Agents' role in this first model consists in wandering around the Sugarscape harvesting the greatest amount of sugar they can find.
 
 # ### Environment
 
@@ -20,8 +20,8 @@
 
 # The Sugarscape obbeys the following rule:
 
-# Sugarscape growback rule G$\alpha$:  
-#     At each cell, sugar grows back at a rate of $\alpha$ units per time-step up to the cell's capacity c. 
+# Sugarscape growback rule G$\alpha$:
+#     At each cell, sugar grows back at a rate of $\alpha$ units per time-step up to the cell's capacity c.
 
 # ### Agents
 
@@ -29,7 +29,7 @@
 
 # * A vision _v_, which is the maximum number of cells the agent can see in each of the four principal lattice directions: north, south, east and west.
 # * A metabolic rate _m_, which represents the units of sugar the agent burns per time-step.
-# * A maximum age _max-age_, which is the maximum number of time-steps the agent can live. 
+# * A maximum age _max-age_, which is the maximum number of time-steps the agent can live.
 
 # Agents also have the capacity to accumulate sugar wealth _w_. An agent's sugar wealth is incremented at the end of each time-step by the sugar collected and decremented by the agent's metabolic rate. __Two agents are not allowed to occupy the same cell in the grid.__
 
@@ -41,7 +41,7 @@
 
 # #### Agent replacement rule _R_:
 
-# Whenever an agent dies it is replaced by a new agent of age 0 placed on a randomly chosen unoccupied cell, having random attributes _v_, _m_ and _max-age_, and random initial wealth w0. All random numbers are drawn from uniform distributions with ranges specified in Table 1 below. 
+# Whenever an agent dies it is replaced by a new agent of age 0 placed on a randomly chosen unoccupied cell, having random attributes _v_, _m_ and _max-age_, and random initial wealth w0. All random numbers are drawn from uniform distributions with ranges specified in Table 1 below.
 
 # ### Scheduling of events
 
@@ -71,66 +71,81 @@ using AgentsPlots
 using Random
 
 mutable struct SugarSeeker <: AbstractAgent
-  id::Int
-  pos::Tuple{Int, Int}
-  vision::Int
-  metabolic_rate::Int
-  age::Int
-  max_age::Int
-  wealth::Int
+    id::Int
+    pos::Tuple{Int,Int}
+    vision::Int
+    metabolic_rate::Int
+    age::Int
+    max_age::Int
+    wealth::Int
 end
 
 # Functions `distances` and `sugar_caps` produce a matrix for the distribution of sugar capacities."
 
 function distances(pos, sugar_peaks, max_sugar)
-  all_dists = Array{Int, 1}(undef, length(sugar_peaks))
-  for (ind, peak) in enumerate(sugar_peaks)
-    d = round(Int, sqrt(sum((pos .- peak).^2)))
-    all_dists[ind] = d
-  end
-  return minimum(all_dists)
+    all_dists = Array{Int,1}(undef, length(sugar_peaks))
+    for (ind, peak) in enumerate(sugar_peaks)
+        d = round(Int, sqrt(sum((pos .- peak) .^ 2)))
+        all_dists[ind] = d
+    end
+    return minimum(all_dists)
 end
 
-function sugar_caps(dims, sugar_peaks, max_sugar, dia=4)
-  sugar_capacities = zeros(Int, dims)
-  for i in 1:dims[1], j in 1:dims[2]
-    sugar_capacities[i, j] = distances((i,j), sugar_peaks, max_sugar)
-  end
-  for i in 1:dims[1]
-    for j in 1:dims[2]
-      sugar_capacities[i, j] = max(0, max_sugar - (sugar_capacities[i, j] ÷ dia))
+function sugar_caps(dims, sugar_peaks, max_sugar, dia = 4)
+    sugar_capacities = zeros(Int, dims)
+    for i in 1:dims[1], j in 1:dims[2]
+        sugar_capacities[i, j] = distances((i, j), sugar_peaks, max_sugar)
     end
-  end
-  return sugar_capacities
+    for i in 1:dims[1]
+        for j in 1:dims[2]
+            sugar_capacities[i, j] = max(0, max_sugar - (sugar_capacities[i, j] ÷ dia))
+        end
+    end
+    return sugar_capacities
 end
 
 "Start a sugarscape simulation"
-function sugarscape(;dims=(50,50), sugar_peaks=((10, 40), (40, 10)), growth_rate=1, N=250, w0_dist=(5, 25), metabolic_rate_dist=(1,4), vision_dist=(1,6), 	max_age_dist=(60, 100), max_sugar = 4)
-  sugar_capacities = sugar_caps(dims, sugar_peaks, max_sugar, 6)
-  sugar_values = deepcopy(sugar_capacities)
-  space = GridSpace(dims, periodic=true, moore=true)
-  properties = Dict(
-  :growth_rate => growth_rate,
-  :N => N,
-  :w0_dist => w0_dist,
-  :metabolic_rate_dist => metabolic_rate_dist,
-  :vision_dist => vision_dist,
-  :max_age_dist => max_age_dist,
-  :sugar_values => sugar_values,
-  :sugar_capacities => sugar_capacities
-  )
-  model = AgentBasedModel(SugarSeeker, space, scheduler=random_activation,
-  properties=properties)
-  for ag in 1:N
-    add_agent_single!(model, 
-    rand(vision_dist[1]:vision_dist[2]),
-    rand(metabolic_rate_dist[1]:metabolic_rate_dist[2]),
-    0,
-    rand(max_age_dist[1]:max_age_dist[2]),
-    rand(w0_dist[1]:w0_dist[2]),
+function sugarscape(;
+    dims = (50, 50),
+    sugar_peaks = ((10, 40), (40, 10)),
+    growth_rate = 1,
+    N = 250,
+    w0_dist = (5, 25),
+    metabolic_rate_dist = (1, 4),
+    vision_dist = (1, 6),
+    max_age_dist = (60, 100),
+    max_sugar = 4,
+)
+    sugar_capacities = sugar_caps(dims, sugar_peaks, max_sugar, 6)
+    sugar_values = deepcopy(sugar_capacities)
+    space = GridSpace(dims, periodic = true, moore = true)
+    properties = Dict(
+        :growth_rate => growth_rate,
+        :N => N,
+        :w0_dist => w0_dist,
+        :metabolic_rate_dist => metabolic_rate_dist,
+        :vision_dist => vision_dist,
+        :max_age_dist => max_age_dist,
+        :sugar_values => sugar_values,
+        :sugar_capacities => sugar_capacities,
     )
-  end
-  return model
+    model = AgentBasedModel(
+        SugarSeeker,
+        space,
+        scheduler = random_activation,
+        properties = properties,
+    )
+    for ag in 1:N
+        add_agent_single!(
+            model,
+            rand(vision_dist[1]:vision_dist[2]),
+            rand(metabolic_rate_dist[1]:metabolic_rate_dist[2]),
+            0,
+            rand(max_age_dist[1]:max_age_dist[2]),
+            rand(w0_dist[1]:w0_dist[2]),
+        )
+    end
+    return model
 end
 
 model = sugarscape()
@@ -138,91 +153,103 @@ model = sugarscape()
 # Fig. 1: Spatial distribution of sugar capacities in the Sugarscape. Cells are coloured according to their sugar capacity.
 
 heatmap(model.sugar_capacities)
-save("capacities.jpg")
+savefig("capacities.jpg")
+
+#
 
 function env!(model)
-  ## At each cell, sugar grows back at a rate of $\alpha$ units per time-step up to the cell's capacity c.
-  togrow = findall(x -> model.sugar_values[x] < model.sugar_capacities[x], 1:prod(model.space.dimensions))
-  model.sugar_values[togrow] .+= model.growth_rate
+    ## At each cell, sugar grows back at a rate of $\alpha$ units per time-step up to the cell's capacity c.
+    togrow = findall(
+        x -> model.sugar_values[x] < model.sugar_capacities[x],
+        1:prod(model.space.dimensions),
+    )
+    model.sugar_values[togrow] .+= model.growth_rate
 end
 
 function movement!(agent, model)
-  posvertex = coord2vertex(agent.pos, model)
-  newsite = posvertex
-  ## find all unoccupied cells within vision
-  neighbors = node_neighbors(coord2vertex(agent.pos, model), model, agent.vision)
-  empty_nodes = [i for i in neighbors if isempty(i, model)]
-  if length(empty_nodes) > 0
-    ## identify the one(s) with greatest amount of sugar
-    maxsugar = maximum(model.sugar_values[empty_nodes])
-    if maxsugar > 0
-      sugary_sites_inds = findall(x -> x == maxsugar, model.sugar_values[empty_nodes])
-      sugary_sites = empty_nodes[sugary_sites_inds]
-      ## select the nearest one (randomly if more than one)
-      for dia in 1:agent.vision
-        nn = node_neighbors(posvertex, model, dia)
-        suitable = intersect(nn, sugary_sites)
-        if length(suitable) > 0
-          newsite = rand(suitable)
-          break
+    posvertex = coord2vertex(agent.pos, model)
+    newsite = posvertex
+    ## find all unoccupied cells within vision
+    neighbors = node_neighbors(coord2vertex(agent.pos, model), model, agent.vision)
+    empty_nodes = [i for i in neighbors if isempty(i, model)]
+    if length(empty_nodes) > 0
+        ## identify the one(s) with greatest amount of sugar
+        maxsugar = maximum(model.sugar_values[empty_nodes])
+        if maxsugar > 0
+            sugary_sites_inds = findall(x -> x == maxsugar, model.sugar_values[empty_nodes])
+            sugary_sites = empty_nodes[sugary_sites_inds]
+            ## select the nearest one (randomly if more than one)
+            for dia in 1:(agent.vision)
+                nn = node_neighbors(posvertex, model, dia)
+                suitable = intersect(nn, sugary_sites)
+                if length(suitable) > 0
+                    newsite = rand(suitable)
+                    break
+                end
+            end
+            ## move there and collect all the sugar in it
+            newsite != posvertex && move_agent!(agent, newsite, model)
         end
-      end
-      ## move there and collect all the sugar in it
-      newsite != posvertex && move_agent!(agent, newsite, model)
     end
-  end
-  ## update wealth (collected - consumed)
-  agent.wealth += (model.sugar_values[newsite] - agent.metabolic_rate)
-  model.sugar_values[newsite] = 0
-  ## age
-  agent.age += 1
+    ## update wealth (collected - consumed)
+    agent.wealth += (model.sugar_values[newsite] - agent.metabolic_rate)
+    model.sugar_values[newsite] = 0
+    ## age
+    agent.age += 1
 end
 
 function replacement!(agent, model)
-  ## If the agent's sugar wealth become zero or less, it dies
-  if agent.wealth <= 0 || agent.age >= agent.max_age
-    kill_agent!(agent, model)
-    ## Whenever an agent dies, a young one is added to a random pos.
-    ## New agent has random attributes
-    add_agent_single!(model, 
-    rand(model.vision_dist[1]:model.vision_dist[2]),
-    rand(model.metabolic_rate_dist[1]:model.metabolic_rate_dist[2]),
-    0,
-    rand(model.max_age_dist[1]:model.max_age_dist[2]),
-    rand(model.w0_dist[1]:model.w0_dist[2]),
-    )
-  end
+    ## If the agent's sugar wealth become zero or less, it dies
+    if agent.wealth <= 0 || agent.age >= agent.max_age
+        kill_agent!(agent, model)
+        ## Whenever an agent dies, a young one is added to a random pos.
+        ## New agent has random attributes
+        add_agent_single!(
+            model,
+            rand(model.vision_dist[1]:model.vision_dist[2]),
+            rand(model.metabolic_rate_dist[1]:model.metabolic_rate_dist[2]),
+            0,
+            rand(model.max_age_dist[1]:model.max_age_dist[2]),
+            rand(model.w0_dist[1]:model.w0_dist[2]),
+        )
+    end
 end
 
 function agent_step!(agent, model)
-  movement!(agent, model)
-  replacement!(agent, model)
+    movement!(agent, model)
+    replacement!(agent, model)
 end
 
 # The following animation shows the emergent unequal distribution of agents on resourceful areas.
 
 anim = @animate for i in 1:50
-  step!(model, agent_step!, env!, 1)
-  p1 = heatmap(model.sugar_values)
-  p2 = plotabm(model, as=3, am=:square, ac=:blue)
-  title!(p1, "Sugar levels")
-  title!(p2, "Agents\n Step $i")
-  p = plot(p1, p2)
+    step!(model, agent_step!, env!, 1)
+    p1 = heatmap(model.sugar_values)
+    p2 = plotabm(model, as = 3, am = :square, ac = :blue)
+    title!(p1, "Sugar levels")
+    title!(p2, "Agents\n Step $i")
+    p = plot(p1, p2)
 end
-gif(anim, "sugar.gif", fps=8)
+gif(anim, "sugar.gif", fps = 8)
 
 # ### Distribution of wealth across individuals
 
 model2 = sugarscape()
-adata, _ = run!(model2, agent_step!, env!, 20, adata=[:wealth])
+adata, _ = run!(model2, agent_step!, env!, 20, adata = [:wealth])
 
 anim2 = @animate for i in 0:20
-  histogram(adata[adata.step .== i, :wealth], legend=false, color=:black, nbins=15, title="step $i")
+    histogram(
+        adata[adata.step .== i, :wealth],
+        legend = false,
+        color = :black,
+        nbins = 15,
+        title = "step $i",
+    )
 end
 
 # We see that the distribution of wealth shifts from a more or less uniform distribution to a skewed distribution.
 
-gif(anim2, fps=3)
+gif(anim2, fps = 3)
 
 # ## References
 
@@ -233,3 +260,4 @@ gif(anim2, fps=3)
 # EPSTEIN J M & Axtell R L (1996) Growing Artificial Societies: Social Science from the Bottom Up. The MIT Press.
 
 # FLENTGE F, Polani D & Uthmann T (2001) Modelling the emergence of possession norms using memes. Journal of Artificial Societies and Social Simulation 4(4)3. http://jasss.soc.surrey.ac.uk/4/4/3.html.
+
