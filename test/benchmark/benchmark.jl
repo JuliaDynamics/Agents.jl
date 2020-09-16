c702687e9a40932303c0532f90e6652b7671ef3d
Ali R. Vahdati <arv.kavir@gmail.com>
2020-09-16 12:00:03 +0200

    Increase @benchmark seconds
    Makes sure all replicates are run in Julia benchmarks

--- test/benchmark/benchmark.jl
+++ test/benchmark/benchmark.jl
@@ -3,8 +3,8 @@ using BenchmarkTools
 using Random
 Random.seed!(10)
 
-a = @benchmark step!(model, agent_step!, model_step!, 200) setup=((model, agent_step!, model_step!) = Models.predator_prey()) samples=100
+a = @benchmark step!(model, agent_step!, model_step!, 200) setup=((model, agent_step!, model_step!) = Models.predator_prey()) samples=100 seconds=2*60
 println("Agents.jl WolfSheep (ms): ", minimum(a.times)*1e-6)
 
-a = @benchmark step!(model, agent_step!, model_step!, 1000) setup=((model, agent_step!, model_step!) = Models.flocking()) samples=100
+a = @benchmark step!(model, agent_step!, model_step!, 1000) setup=((model, agent_step!, model_step!) = Models.flocking()) samples=100  seconds=2*60
 println("Agents.jl Flocking (ms): ", minimum(a.times)*1e-6)
