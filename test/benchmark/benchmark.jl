9ee4f2d59d0031c1dd93e74305ab51915061133b
Ali R. Vahdati <arv.kavir@gmail.com>
2020-09-16 09:29:03 +0200

    Add 100 replicates for julia benchmarks

--- test/benchmark/benchmark.jl
+++ test/benchmark/benchmark.jl
@@ -4,9 +4,9 @@ using Random
 Random.seed!(10)
 
 println("Wolf Sheep Grass")
-a = @benchmark step!(model, agent_step!, model_step!, 500) setup=((model, agent_step!, model_step!) = Models.predator_prey())
+a = @benchmark step!(model, agent_step!, model_step!, 500) setup=((model, agent_step!, model_step!) = Models.predator_prey()) samples=100
 display(a)
 
 println("Flocking")
-a = @benchmark step!(model, agent_step!, model_step!, 1000) setup=((model, agent_step!, model_step!) = Models.flocking())
+a = @benchmark step!(model, agent_step!, model_step!, 1000) setup=((model, agent_step!, model_step!) = Models.flocking()) samples=100
 display(a)
