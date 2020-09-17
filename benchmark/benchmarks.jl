2c0c604616892c95a755f20ba39a2e8f625375b4
Tim DuBois <tim@neophilus.net>
2020-09-17 21:09:58 +0200

    Revert "Instantiate model outside add_agent! benchmarks"
    This reverts commit f1e9a9222ee7a554014ca40a7e87523de76abfbc.

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -208,14 +208,17 @@ SUITE["grid"]["node"]["agents"] =
 #### API -> CONTINUOUS ####
 
 continuous_model = ABM(ContinuousAgent, ContinuousSpace(3; extend = (10.0, 10.0, 10.0)))
+continuous_agent = ContinuousAgent(1, (2.2, 1.9, 7.5), (0.5, 1.0, 0.01), 6.5, false)
+
+# We must create the model inside our benchmark call here, otherwise we hit the issue from #226.
 
 SUITE["continuous"]["add"]["agent"] = @benchmarkable add_agent!(
-    ContinuousAgent(1, (2.2, 1.9, 7.5), (0.5, 1.0, 0.01), 6.5, false),
-    $continuous_model,
+    $continuous_agent,
+    ABM(ContinuousAgent, ContinuousSpace(3; extend = (10.0, 10.0, 10.0))),
 )
 SUITE["continuous"]["add"]["agent_pos"] = @benchmarkable add_agent_pos!(
-    ContinuousAgent(2, (2.2, 1.9, 7.5), (0.5, 1.0, 0.01), 6.5, false),
-    $continuous_model,
+    $continuous_agent,
+    ABM(ContinuousAgent, ContinuousSpace(3; extend = (10.0, 10.0, 10.0))),
 )
 SUITE["continuous"]["add"]["create_pos"] = @benchmarkable add_agent!(
     (5.8, 3.5, 9.4),
@@ -227,7 +230,9 @@ SUITE["continuous"]["add"]["create_pos"] = @benchmarkable add_agent!(
 SUITE["continuous"]["add"]["create"] =
     @benchmarkable add_agent!($continuous_model, (0.1, 0.7, 0.2), 6.5, false)
 
-continuous_model2 = ABM(
+SUITE["continuous"]["add_union"]["agent"] = @benchmarkable add_agent!(
+    $continuous_agent,
+    ABM(
         Union{
             ContinuousAgent,
             ContinuousAgentTwo,
@@ -235,12 +240,24 @@ continuous_model2 = ABM(
             ContinuousAgentFour,
             ContinuousAgentFive,
         },
-        ContinuousSpace(3; extend = (10.0, 10.0, 10.0)), warn=false)
-
-SUITE["continuous"]["add_union"]["agent"] = @benchmarkable add_agent!(
-    ContinuousAgent(1, (2.2, 1.9, 7.5), (0.5, 1.0, 0.01), 6.5, false), $continuous_model2)
+        ContinuousSpace(3; extend = (10.0, 10.0, 10.0));
+        warn = false,
+    ),
+)
 SUITE["continuous"]["add_union"]["agent_pos"] = @benchmarkable add_agent_pos!(
-    ContinuousAgent(2, (2.2, 1.9, 7.5), (0.5, 1.0, 0.01), 6.5, false), $continuous_model2)
+    $continuous_agent,
+    ABM(
+        Union{
+            ContinuousAgent,
+            ContinuousAgentTwo,
+            ContinuousAgentThree,
+            ContinuousAgentFour,
+            ContinuousAgentFive,
+        },
+        ContinuousSpace(3; extend = (10.0, 10.0, 10.0));
+        warn = false,
+    ),
+)
 
 for _ in 1:500
     add_agent!(continuous_model, (0.8, 0.7, 1.3), 6.5, false)
