b34429ca9c4ec4cca49ee0c6298a33e574e0afdc
Tim DuBois <tim@neophilus.net>
2020-09-17 11:08:59 +0200

    Expand space, make sure test is just each function, extend union size

---
+++ benchmark/agents.jl
@@ -0,0 +1,80 @@
+mutable struct GraphAgent <: AbstractAgent
+    id::Int
+    pos::Int
+end
+
+mutable struct GraphAgentTwo <: AbstractAgent
+    id::Int
+    pos::Int
+end
+
+mutable struct GraphAgentThree <: AbstractAgent
+    id::Int
+    pos::Int
+end
+
+mutable struct GraphAgentFour <: AbstractAgent
+    id::Int
+    pos::Int
+end
+
+mutable struct GraphAgentFive <: AbstractAgent
+    id::Int
+    pos::Int
+end
+
+mutable struct GridAgent <: AbstractAgent
+    id::Int
+    pos::Tuple{Int,Int}
+end
+
+mutable struct GridAgentTwo <: AbstractAgent
+    id::Int
+    pos::Tuple{Int,Int}
+end
+
+mutable struct GridAgentThree <: AbstractAgent
+    id::Int
+    pos::Tuple{Int,Int}
+end
+
+mutable struct GridAgentFour <: AbstractAgent
+    id::Int
+    pos::Tuple{Int,Int}
+end
+
+mutable struct GridAgentFive <: AbstractAgent
+    id::Int
+    pos::Tuple{Int,Int}
+end
+
+mutable struct ContinuousAgent <: AbstractAgent
+    id::Int
+    pos::NTuple{2,Float64}
+    vel::NTuple{2,Float64}
+end
+
+mutable struct ContinuousAgentTwo <: AbstractAgent
+    id::Int
+    pos::NTuple{2,Float64}
+    vel::NTuple{2,Float64}
+end
+
+mutable struct ContinuousAgentThree <: AbstractAgent
+    id::Int
+    pos::NTuple{2,Float64}
+    vel::NTuple{2,Float64}
+end
+
+mutable struct ContinuousAgentFour <: AbstractAgent
+    id::Int
+    pos::NTuple{2,Float64}
+    vel::NTuple{2,Float64}
+end
+
+mutable struct ContinuousAgentFive <: AbstractAgent
+    id::Int
+    pos::NTuple{2,Float64}
+    vel::NTuple{2,Float64}
+end
+
--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -2,40 +2,11 @@ using BenchmarkTools, Agents, LightGraphs
 
 const SUITE = BenchmarkGroup(["Agents"])
 
-mutable struct GraphAgent <: AbstractAgent
-    id::Int
-    pos::Int
-end
+include("agents.jl")
 
-mutable struct GraphAgentTwo <: AbstractAgent
-    id::Int
-    pos::Int
-end
-
-mutable struct GridAgent <: AbstractAgent
-    id::Int
-    pos::Tuple{Int,Int}
-end
-
-mutable struct GridAgentTwo <: AbstractAgent
-    id::Int
-    pos::Tuple{Int,Int}
-end
-
-mutable struct ContinuousAgent <: AbstractAgent
-    id::Int
-    pos::NTuple{2,Float64}
-    vel::NTuple{2,Float64}
-end
-
-mutable struct ContinuousAgentTwo <: AbstractAgent
-    id::Int
-    pos::NTuple{2,Float64}
-    vel::NTuple{2,Float64}
-end
-
-grid_model = ABM(GridAgent, GridSpace((10, 10)))
-grid_union_model = ABM(GridAgent, GridSpace((10, 10)))
+grid_model = ABM(GridAgent, GridSpace((1000, 1000)))
+grid_agent = GridAgent(1, (2,3))
+grid_union_model = ABM(Union{GridAgent, GridAgentTwo, GridAgentThree, GridAgentFour, GridAgentFive}, GridSpace((1000, 1000)); warn=false)
 
 SUITE["space"] = BenchmarkGroup(["graph", "grid", "continuous"])
 SUITE["space"]["graph"] = @benchmarkable GraphSpace(complete_digraph(1000))
@@ -52,13 +23,26 @@ SUITE["model"]["initialise_union"]["graph"] = @benchmarkable ABM(Union{GraphAgen
 SUITE["model"]["initialise_union"]["grid"] = @benchmarkable ABM(Union{GridAgent, GridAgentTwo}, GridSpace((10, 10)); warn=false)
 SUITE["model"]["initialise_union"]["continuous"] = @benchmarkable ABM(Union{ContinuousAgent, ContinuousAgentTwo}, ContinuousSpace(3); warn=false)
 
-SUITE["grid"] = BenchmarkGroup(["add"])
+SUITE["grid"] = BenchmarkGroup(["add","add_union"])
 SUITE["grid"]["add"] = BenchmarkGroup(["agent", "agent_pos", "agent_single", "create", "create_pos", "create_single"])
-SUITE["grid"]["add"]["agent"] = @benchmarkable add_agent!(GridAgent(1, (2,3)), $grid_model)
-SUITE["grid"]["add"]["agent_pos"] = @benchmarkable add_agent_pos!(GridAgent(1, (2,3)), $grid_model)
-SUITE["grid"]["add"]["agent_single"] = @benchmarkable add_agent_single!(GridAgent(1, (2,3)), $grid_model)
-# Ready for current master
-#SUITE["grid"]["add"]["agent_union"] = @benchmarkable add_agent!(GridAgent, $grid_union_model)
+SUITE["grid"]["add"]["agent"] = @benchmarkable add_agent!(grid_agent, $grid_model)
+# We genocide everything between benchmarks to ensure agents are 'added' and not overwritten
+# (also, the add_agent_single! calls need an empty model)
+genocide!(grid_model)
+SUITE["grid"]["add"]["agent_pos"] = @benchmarkable add_agent_pos!(grid_agent, $grid_model)
+genocide!(grid_model)
+SUITE["grid"]["add"]["agent_single"] = @benchmarkable add_agent_single!(grid_agent, $grid_model)
+genocide!(grid_model)
 SUITE["grid"]["add"]["create"] = @benchmarkable add_agent!($grid_model)
+genocide!(grid_model)
 SUITE["grid"]["add"]["create_pos"] = @benchmarkable add_agent!((1,3), $grid_model)
+genocide!(grid_model)
 SUITE["grid"]["add"]["create_single"] = @benchmarkable add_agent_single!($grid_model)
+SUITE["grid"]["add_union"] = BenchmarkGroup(["agent", "agent_pos", "agent_single"])
+# Think this is only on current master, will need to rebase
+#SUITE["grid"]["add"]["agent_union"] = @benchmarkable add_agent!(GridAgent, $grid_union_model)
+SUITE["grid"]["add_union"]["agent"] = @benchmarkable add_agent!(grid_agent, $grid_union_model)
+genocide!(grid_union_model)
+SUITE["grid"]["add_union"]["agent_pos"] = @benchmarkable add_agent_pos!(grid_agent, $grid_union_model)
+genocide!(grid_union_model)
+SUITE["grid"]["add_union"]["agent_single"] = @benchmarkable add_agent_single!(grid_agent, $grid_union_model)
