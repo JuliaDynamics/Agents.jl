42069d0f9fc9182b376092d2f6e47192fff9db5f
Tim DuBois <tim@neophilus.net>
2020-09-19 13:29:51 +0200

    GraphSpace display issue fixed

--- src/spaces/graph.jl
+++ src/spaces/graph.jl
@@ -23,33 +23,42 @@ end
     nv(model::ABM)
 Return the number of nodes (vertices) in the `model` space.
 """
-LightGraphs.nv(abm::ABM{<:Any, <:GraphSpace}) = LightGraphs.nv(abm.space.graph)
+LightGraphs.nv(abm::ABM{<:Any,<:GraphSpace}) = LightGraphs.nv(abm.space.graph)
 
 """
     ne(model::ABM)
 Return the number of edges in the `model` space.
 """
-LightGraphs.ne(abm::ABM{<:Any, <:GraphSpace}) = LightGraphs.ne(abm.space.graph)
+LightGraphs.ne(abm::ABM{<:Any,<:GraphSpace}) = LightGraphs.ne(abm.space.graph)
 
-function Base.show(io::IO, abm::GraphSpace)
-    s = "$(nameof(typeof(abm))) with $(nv(abm)) nodes and $(ne(abm)) edges"
-    print(io, s)
+function Base.show(io::IO, space::GraphSpace)
+    print(
+        io,
+        "$(nameof(typeof(space))) with $(nv(space.graph)) nodes and $(ne(space.graph)) edges",
+    )
 end
 
 
 #######################################################################################
 # Agents.jl space API
 #######################################################################################
-random_position(model::ABM{<:AbstractAgent, <:GraphSpace}) = rand(1:nv(model))
+random_position(model::ABM{<:AbstractAgent,<:GraphSpace}) = rand(1:nv(model))
 
-function remove_agent_from_space!(agent::A, model::ABM{A,<:GraphSpace}) where {A<:AbstractAgent}
+function remove_agent_from_space!(
+    agent::A,
+    model::ABM{A,<:GraphSpace},
+) where {A<:AbstractAgent}
     agentnode = agent.pos
     p = get_node_contents(agentnode, model)
     splice!(p, findfirst(a -> a == agent.id, p))
     return model
 end
 
-function move_agent!(agent::A, pos::ValidPos, model::ABM{A,<:GraphSpace}) where {A<:AbstractAgent}
+function move_agent!(
+    agent::A,
+    pos::ValidPos,
+    model::ABM{A,<:GraphSpace},
+) where {A<:AbstractAgent}
     oldnode = agent.pos, model
     p = get_node_contents(oldnode, model)
     splice!(p, findfirst(a -> a == agent.id, p))
@@ -58,7 +67,10 @@ function move_agent!(agent::A, pos::ValidPos, model::ABM{A,<:GraphSpace}) where 
     return agent
 end
 
-function add_agent_to_space!(agent::A, model::ABM{A,<:DiscreteSpace}) where {A<:AbstractAgent}
+function add_agent_to_space!(
+    agent::A,
+    model::ABM{A,<:DiscreteSpace},
+) where {A<:AbstractAgent}
     push!(get_node_contents(agent.pos, model), agent.id)
     return agent
 end
@@ -72,35 +84,43 @@ Return the ids of agents in the "node" corresponding to `position`.
 get_node_contents(n::Integer, model::ABM{A,<:GraphSpace}) where {A} = model.space.s[n]
 # NOTICE: The return type of `get_node_contents` must support `length` and `isempty`!
 
-nodes(model::ABM{<:AbstractAgent, GraphSpace}) = 1:nv(model)
+nodes(model::ABM{<:AbstractAgent,GraphSpace}) = 1:nv(model)
 
 #######################################################################################
 # Neighbors
 #######################################################################################
-function space_neighbors(pos::Int, model::ABM{A, <: GraphSpace}, args...; kwargs...) where {A}
-  nn = node_neighbors(pos, model, args...; kwargs...)
-  # TODO: Use flatten here or something for performance?
-  # `model.space.s[nn]...` allocates a lot
-  # We include the current node in the search since we are searching over space
-  vcat(model.space.s[pos], model.space.s[nn]...)
+function space_neighbors(pos::Int, model::ABM{A,<:GraphSpace}, args...; kwargs...) where {A}
+    nn = node_neighbors(pos, model, args...; kwargs...)
+    # TODO: Use flatten here or something for performance?
+    # `model.space.s[nn]...` allocates a lot
+    # We include the current node in the search since we are searching over space
+    vcat(model.space.s[pos], model.space.s[nn]...)
 end
 
-function node_neighbors(node_number::Integer, model::ABM{A, <: GraphSpace}; neighbor_type::Symbol=:default) where {A}
+function node_neighbors(
+    node_number::Integer,
+    model::ABM{A,<:GraphSpace};
+    neighbor_type::Symbol = :default,
+) where {A}
     @assert neighbor_type ∈ (:default, :all, :in, :out)
-    neighborfn =
-        if neighbor_type == :default
-            LightGraphs.neighbors
-        elseif neighbor_type == :in
-            LightGraphs.inneighbors
-        elseif neighbor_type == :out
-            LightGraphs.outneighbors
-        else
-            LightGraphs.all_neighbors
-        end
+    neighborfn = if neighbor_type == :default
+        LightGraphs.neighbors
+    elseif neighbor_type == :in
+        LightGraphs.inneighbors
+    elseif neighbor_type == :out
+        LightGraphs.outneighbors
+    else
+        LightGraphs.all_neighbors
+    end
     neighborfn(model.space.graph, node_number)
 end
 
-function node_neighbors(node_number::Integer, model::ABM{A, <: GraphSpace}, radius::Integer; kwargs...) where {A}
+function node_neighbors(
+    node_number::Integer,
+    model::ABM{A,<:GraphSpace},
+    radius::Integer;
+    kwargs...,
+) where {A}
     neighbor_nodes = Set(node_neighbors(node_number, model; kwargs...))
     included_nodes = Set()
     for rad in 2:radius
@@ -118,9 +138,10 @@ function node_neighbors(node_number::Integer, model::ABM{A, <: GraphSpace}, radi
         end
     end
     nlist = collect(neighbor_nodes)
-    j = findfirst(a-> a==node_number, nlist)
+    j = findfirst(a -> a == node_number, nlist)
     if j != nothing
         deleteat!(nlist, j)
     end
     return nlist
 end
+
