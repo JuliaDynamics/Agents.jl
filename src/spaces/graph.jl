1c54726ccec39a7078d36ad7011c18bd4c9b0aa5
Tim DuBois <tim@neophilus.net>
2020-09-20 14:58:57 +0200

    Graph neighbor improvements

--- src/spaces/graph.jl
+++ src/spaces/graph.jl
@@ -91,11 +91,7 @@ nodes(model::ABM{<:AbstractAgent,<:GraphSpace}) = 1:nv(model)
 #######################################################################################
 function space_neighbors(pos::Int, model::ABM{A,<:GraphSpace}, args...; kwargs...) where {A}
     nn = node_neighbors(pos, model, args...; kwargs...)
-    # TODO: Use flatten here or something for performance?
-    # `model.space.s[nn]...` allocates, because it creates a new array!
-    # Also `vcat` allocates a second time
-    # We include the current node in the search since we are searching over space
-    vcat(model.space.s[pos], model.space.s[nn]...)
+    Iterators.flatten(model.space.s[i] for i in Iterators.flatten((pos,nn)))
 end
 
 function node_neighbors(
@@ -113,7 +109,7 @@ function node_neighbors(
     else
         LightGraphs.all_neighbors
     end
-    neighborfn(model.space.graph, node_number)
+    Base.Generator(Int, neighborfn(model.space.graph, node_number))
 end
 
 function node_neighbors(
@@ -123,25 +119,9 @@ function node_neighbors(
     kwargs...,
 ) where {A}
     neighbor_nodes = Set(node_neighbors(node_number, model; kwargs...))
-    included_nodes = Set()
-    for rad in 2:radius
-        templist = Vector{Int}()
-        for nn in neighbor_nodes
-            if !in(nn, included_nodes)
-                newns = node_neighbors(nn, model; kwargs...)
-                for newn in newns
-                    push!(templist, newn)
-                end
-            end
-        end
-        for tt in templist
-            push!(neighbor_nodes, tt)
-        end
+    for _ in 2:radius
+        newnns = (node_neighbors(nn, model; kwargs...) for nn in neighbor_nodes)
+        union!(neighbor_nodes, newnns...)
     end
-    nlist = collect(neighbor_nodes)
-    j = findfirst(a -> a == node_number, nlist)
-    if j != nothing
-        deleteat!(nlist, j)
-    end
-    return nlist
+    Iterators.filter(i->i!=node_number, Base.Generator(Int, neighbor_nodes))
 end
