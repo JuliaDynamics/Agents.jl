c167684de831857a17fbca39172e105a99c60c0c
Ali R. Vahdati <arv.kavir@gmail.com>
2020-09-24 17:06:31 +0200

    add move_agent! for compartment space

--- src/spaces/compartment.jl
+++ src/spaces/compartment.jl
@@ -6,25 +6,28 @@ struct CompartmentSpace{D,F} <: AbstractSpace
   periodic::Bool
   metric::Symbol
   dims::NTuple{D, Int}
+  extent::NTuple{D, Int}
+  D::Int
 end
 
 defvel(a, m) = nothing
 
-function CompartmentSpace(d::NTuple{D,Real}, spacing; update_vel! = defvel,
-  periodic = true, metric = :cityblock) where {D}
+function CompartmentSpace(d::NTuple{D,Real}, spacing;
+  update_vel! = defvel, periodic = true, metric = :cityblock,
+  extent = Tuple(1.0 for i in 1:D)) where {D}
+  
   @assert metric ∈ (:cityblock, :euclidean)
-
   s = Array{Vector{Int},D}(undef, round.(Int, d ./ spacing))
   for i in eachindex(s)
-      s[i] = Int[]
+    s[i] = Int[]
   end
-  return CompartmentSpace{D,typeof(update_vel!)}(s, update_vel!, periodic, metric, size(s))
+  return CompartmentSpace{D,typeof(update_vel!)}(s, update_vel!, periodic, metric, size(s), extent, D)
 end
 
 function Base.show(io::IO, space::CompartmentSpace)
-    s = "$(join(space.dims, "×")) $(space.periodic ? "periodic " : "")continuous space"
-    space.update_vel! ≠ defvel && (s *= " with velocity updates")
-    print(io, s)
+  s = "$(space.periodic ? "periodic" : "") continuous space on with $(join(space.dims, "×")) divisions in ranges $(Tuple(zeros(length(space.extent)))) to $(space.extent)"
+  space.update_vel! ≠ defvel && (s *= " with velocity updates")
+  print(io, s)
 end
 
 """
@@ -32,34 +35,65 @@ end
 Return a random position in the model's space (always with appropriate Type).
 """
 function random_position(model::ABM{A, <:CompartmentSpace}) where {A}
-  pos = rand.((:).(1, model.space.dims))
+  pos = Tuple(rand(model.space.D)) .* model.space.extent
 end
 
 """
-    move_agent!(agent [, pos], model::ABM) → agent
+    move_agent!(agent::A, model::ABM{A, CompartmentSpace}, dt = 1.0)
+Propagate the agent forwards one step according to its velocity,
+_after_ updating the agent's velocity (see [`CompartmentSpace`](@ref)).
+Also take care of periodic boundary conditions.
 
-Move agent to the given position, or to a random one if a position is not given.
-`pos` must have the appropriate position type depending on the space type.
+For this continuous space version of `move_agent!`, the "evolution algorithm"
+is a trivial Euler scheme with `dt` the step size, i.e. the agent position is updated
+as `agent.pos += agent.vel * dt`.
 
-The agent's position is updated to match `pos` after the move.
+Notice that if you want the agent to instantly move to a specified position, do
+`move_agent!(agent, pos, model)`.
 """
-move_agent!(agent, pos, model) = notimplemented(model)
+function move_agent!(agent::A, model::ABM{A, <: CompartmentSpace}, dt::Real = 1.0) where {A <: AbstractAgent}
+  model.space.update_vel!(agent, model)
+  pos = agent.pos .+ dt .* agent.vel
+  if model.space.periodic
+    pos = mod.(pos, model.space.extent)
+  end
+  move_agent!(agent, pos, model)
+  return agent.pos
+end
 
 """
-    add_agent_to_space!(agent, model)
-Add the agent to the underlying space structure at the agent's own position.
-This function is called after the agent is already inserted into the model dictionary
-and `maxid` has been updated. This function is NOT part of the public API.
+    move_agent!(agent::A, model::ABM{A, CompartmentSpace}, vel::NTuple{D, N}, dt = 1.0)
+Propagate the agent forwards one step according to `vel` and the model's space, with `dt` as the time step. (`update_vel!` is not used)
 """
-add_agent_to_space!(agent, model) = notimplemented(model)
+function move_agent!(agent::A, model::ABM{A,S,F,P}, vel::NTuple{D, X}, dt = 1.0) where {A <: AbstractAgent, S <: CompartmentSpace, F, P, D, X <: AbstractFloat}
+  pos = agent.pos .+ dt .* vel
+  if model.space.periodic
+    pos = mod.(pos, model.space.extent)
+  end
+  move_agent!(agent, pos, model)
+  return agent.pos
+end
 
-"""
-    remove_agent_from_space!(agent, model)
-Remove the agent from the underlying space structure.
-This function is called after the agent is already removed from the model dictionary
-This function is NOT part of the public API.
-"""
-remove_agent_from_space!(agent, model) = notimplemented(model)
+function move_agent!(a::A, pos::Tuple, model::ABM{A,<:CompartmentSpace}) where {A<:AbstractAgent}
+  remove_agent_from_space!(a, model)
+  a.pos = pos
+  add_agent_to_space!(a, model)
+end
+
+pos_to_cell(pos::Tuple, model) = ceil.(Int, pos .* model.space.dims)
+pos_to_cell(a::A, model) where {A<:AbstractAgent} = pos_to_cell(a.pos, model)
+
+function add_agent_to_space!(a::A, model::ABM{A,<:CompartmentSpace}) where {A<:AbstractAgent}
+    push!(model.space.s[pos_to_cell(a, model)...], a.id)
+    return a
+end
+
+function remove_agent_from_space!(a::A, model::ABM{A,<:CompartmentSpace}) where {A<:AbstractAgent}
+    prev = model.space.s[pos_to_cell(a, model)...]
+    ai = findfirst(i -> i == a.id, prev)
+    deleteat!(prev, ai)
+    return a
+end
 
 #######################################################################################
 # %% IMPLEMENT: Neighbors and stuff
