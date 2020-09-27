bae9fca4a3f3c5c2f0964745ba8fa016da626f31
Ali R. Vahdati <arv.kavir@gmail.com>
2020-09-27 15:07:19 +0200

    Fix bug in exact space neighbors

--- src/spaces/compartment.jl
+++ src/spaces/compartment.jl
@@ -60,84 +60,84 @@ as `agent.pos += agent.vel * dt`.
 
 Notice that if you want the agent to instantly move to a specified position, do
     `move_agent!(agent, pos, model)`.
-    """
-    function move_agent!(agent::A, model::ABM{A, <: CompartmentSpace}, dt::Real = 1.0) where {A <: AbstractAgent}
-        model.space.update_vel!(agent, model)
-        pos = agent.pos .+ dt .* agent.vel
-        move_agent!(agent, pos, model)
-        return agent.pos
-    end
-    
-    #######################################################################################
-    # %% Neighbors and stuff
-    #######################################################################################
-    
-    grid_space_neighborhood(α, model::ABM{<:AbstractAgent, <:CompartmentSpace}, r) =
-    grid_space_neighborhood(α, model.space.grid, r)
-    
-    function nearby_ids(pos::ValidPos, model::ABM{<:AbstractAgent,<:CompartmentSpace}, r = 1)
-        nn = grid_space_neighborhood(CartesianIndex(pos2cell(pos)), model, r)
-        s = model.space.grid.s
-        Iterators.flatten((s[i...] for i in nn))
-    end
-    
-    function nearby_positions(pos::ValidPos, model::ABM{<:AbstractAgent,<:CompartmentSpace}, r = 1)
-        nn = grid_space_neighborhood(CartesianIndex(pos2cell(pos)), model, r)
-        Iterators.filter(!isequal(pos), nn)
-    end
-    
-    function positions(model::ABM{<:AbstractAgent,<:CompartmentSpace})
-        x = CartesianIndices(model.space.grid.s)
-        return (Tuple(y) for y in x)
-    end
-    
-    function ids_in_position(pos::ValidPos, model::ABM{<:AbstractAgent,<:CompartmentSpace})
-        return model.space.grid.s[pos...]
-    end
-    
-    cell_center(pos::Tuple) = getindex.(modf.(pos), 2) .+ 0.5
-    distance_from_cell_center(pos::Tuple, center) = sqrt(sum(abs2.(pos .- center)))
-    
-    """
-    space_neighbors(position, model::ABM, r=1; kwargs...) → ids
-    
-    Return an iterator of the ids of the agents within "radius" `r` of the given `position`
-    (which must match type with the spatial structure of the `model`).
-    """
-    function space_neighbors(pos, model::ABM{<:AbstractAgent,<:CompartmentSpace}, r=1; exact=false)
-        center = cell_center(pos)
-        focal_cell = ceil.(Int, center)
-        if exact
-            newr = ceil.(Int, r)
-            corner_of_largest_square_in_circle = floor.(Int, center .+ newr)
-            max_distance = corner_of_largest_square_in_circle[1] - focal_cell[1]
-            final_ids = Int[] # TODO make it an iterator
-            allcells = nearby_positions(focal_cell, model, newr)
-            for cell in allcells
-                if !any(x-> x> max_distance, abs.(cell .- focal_cell)) # certain cell
-                    ids = ids_in_position(cell, model)
-                    final_ids = vcat(final_ids, ids)
-                else # uncertain cell
-                    ids = ids_in_position(cell, model)
-                    filter!(i -> sqrt(sum(abs2.(center .- model[i]))) ≤ rnew, ids)
-                    final_ids = vcat(final_ids, ids)
-                end
+"""
+function move_agent!(agent::A, model::ABM{A, <: CompartmentSpace}, dt::Real = 1.0) where {A <: AbstractAgent}
+    model.space.update_vel!(agent, model)
+    pos = agent.pos .+ dt .* agent.vel
+    move_agent!(agent, pos, model)
+    return agent.pos
+end
+
+#######################################################################################
+# %% Neighbors and stuff
+#######################################################################################
+
+grid_space_neighborhood(α, model::ABM{<:AbstractAgent, <:CompartmentSpace}, r) =
+grid_space_neighborhood(α, model.space.grid, r)
+
+function nearby_ids(pos::ValidPos, model::ABM{<:AbstractAgent,<:CompartmentSpace}, r = 1)
+    nn = grid_space_neighborhood(CartesianIndex(pos2cell(pos)), model, r)
+    s = model.space.grid.s
+    Iterators.flatten((s[i...] for i in nn))
+end
+
+function nearby_positions(pos::ValidPos, model::ABM{<:AbstractAgent,<:CompartmentSpace}, r = 1)
+    nn = grid_space_neighborhood(CartesianIndex(pos2cell(pos)), model, r)
+    Iterators.filter(!isequal(pos), nn)
+end
+
+function positions(model::ABM{<:AbstractAgent,<:CompartmentSpace})
+    x = CartesianIndices(model.space.grid.s)
+    return (Tuple(y) for y in x)
+end
+
+function ids_in_position(pos::ValidPos, model::ABM{<:AbstractAgent,<:CompartmentSpace})
+    return model.space.grid.s[pos...]
+end
+
+cell_center(pos::Tuple) = getindex.(modf.(pos), 2) .+ 0.5
+distance_from_cell_center(pos::Tuple, center) = sqrt(sum(abs2.(pos .- center)))
+
+"""
+space_neighbors(position, model::ABM, r=1; kwargs...) → ids
+
+Return an iterator of the ids of the agents within "radius" `r` of the given `position`
+(which must match type with the spatial structure of the `model`).
+"""
+function space_neighbors(pos, model::ABM{<:AbstractAgent,<:CompartmentSpace}, r=1; exact=false)
+    center = cell_center(pos)
+    focal_cell = ceil.(Int, center)
+    if exact
+        newr = ceil.(Int, r)
+        corner_of_largest_square_in_circle = floor.(Int, pos .+ newr)
+        max_distance = corner_of_largest_square_in_circle[1] - focal_cell[1]
+        final_ids = Int[] # TODO make it an iterator
+        allcells = nearby_positions(focal_cell, model, newr)
+        for cell in allcells
+            if !any(x-> x> max_distance, abs.(cell .- focal_cell)) # certain cell
+                ids = ids_in_position(cell, model)
+                final_ids = vcat(final_ids, ids)
+            else # uncertain cell
+                ids = ids_in_position(cell, model)
+                filter!(i -> sqrt(sum(abs2.(pos .- model[i]))) ≤ rnew, ids)
+                final_ids = vcat(final_ids, ids)
             end
-            return final_ids
-        else
-            δ = distance_from_cell_center(pos, center)
-            newr = ceil.(Int, r+δ)
-            return nearby_ids(focal_cell, model, newr)
         end
+        return final_ids
+    else
+        δ = distance_from_cell_center(pos, center)
+        newr = ceil.(Int, r+δ)
+        return nearby_ids(focal_cell, model, newr)
     end
+end
     
     
-    ################################################################################
-    ### Pretty printing
-    ################################################################################
-    function Base.show(io::IO, space::CompartmentSpace{D,P}) where {D, P}
-        s = "$(P ? "periodic" : "") continuous space with $(join(space.dims, "×")) divisions"
-        space.update_vel! ≠ defvel && (s *= " with velocity updates")
-        print(io, s)
-    end
+################################################################################
+### Pretty printing
+################################################################################
+function Base.show(io::IO, space::CompartmentSpace{D,P}) where {D, P}
+    s = "$(P ? "periodic" : "") continuous space with $(join(space.dims, "×")) divisions"
+    space.update_vel! ≠ defvel && (s *= " with velocity updates")
+    print(io, s)
+end
     
