924cc0b5ae9353dffc59947560d2607f1c91ac77
Datseris <datseris.george@gmail.com>
2020-09-28 21:30:43 +0200

    plotting code that also shows circle

--- test/plot_compartment.jl
+++ test/plot_compartment.jl
@@ -1,6 +1,7 @@
 # Visually inspect CompartmentSpace `nearby_ids`
 using Agents
 using AgentsPlots
+# using Plots
 
 mutable struct Ag <: AbstractAgent
     id::Int
@@ -13,8 +14,8 @@ for i in 1:800
     add_agent!(model)
 end
 
-global r = 1.4
-global agentid = 299
+r = 1.4
+agentid = rand(1:800)
 function ac(a)
     if a.id == agentid
         return :red
@@ -24,4 +25,15 @@ function ac(a)
         return :yellow
     end
 end
-plotabm(model, as=2.5, ac=ac, grid= (:both, :dot, 1, 0.9), xticks=1:20, yticks=1:20)
+
+function circleShape(h, k, r)
+    θ = LinRange(0, 2π, 500)
+    h .+ r*sin.(θ), k .+ r .* cos.(θ)
+end
+
+p = plotabm(model, as=2.5, ac=ac, grid = (:both, :dot, 1, 0.9), xticks=1:20, yticks=1:20)
+a = model[agentid]
+AgentsPlots.Plots.plot!(p, circleShape(a.pos[1], a.pos[2], r), seriestype = [:shape, ], lw = 0.5,
+      c = :blue, linecolor = :black, legend = false, fillalpha = 0.2, aspect_ratio=1)
+
+p
