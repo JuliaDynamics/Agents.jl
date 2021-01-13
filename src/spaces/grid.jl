7bec9f482b0b7857b14cdec558d8de8aedb41b1f
Tim DuBois <tim@neophilus.net>
2021-01-13 15:00:41 +0100

    Simplify hood init

--- src/spaces/grid.jl
+++ src/spaces/grid.jl
@@ -150,11 +150,7 @@ reducing the amount of computations necessary (i.e. we don't "find" new indices,
 we only add a pre-determined amount of indices to `α`).
 """
 function grid_space_neighborhood(α::CartesianIndex, space::GridSpace, r::Real)
-    hood = if haskey(space.hoods, r)
-        space.hoods[r]
-    else
-        initialize_neighborhood!(space, r)
-    end
+    hood = get(_ -> initialize_neighborhood!(space, r), space.hoods, r)
     _grid_space_neighborhood(α, space, hood)
 end
 
@@ -170,11 +166,7 @@ function grid_space_neighborhood(
     space::GridSpace{D},
     r::NTuple{D,Real},
 ) where {D}
-    hood = if haskey(space.hoods_tuple, r)
-        space.hoods_tuple[r]
-    else
-        initialize_neighborhood!(space, r)
-    end
+    hood = get(_ -> initialize_neighborhood!(space, r), space.hoods_tuple, r)
     _grid_space_neighborhood(α, space, hood)
 end
 
