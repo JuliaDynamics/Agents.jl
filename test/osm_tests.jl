8662db58789447a8d810c819aeaf8f79b4c754d3
Tim DuBois <tim@neophilus.net>
2021-01-21 11:47:24 +0100

    Additional test coverage

--- test/osm_tests.jl
+++ test/osm_tests.jl
@@ -30,6 +30,11 @@
     add_agent!(start_r, model, route, finish_r)
     add_agent!(finish_i, model, [], finish_i)
 
+    @test osm_latlon(model[2], model) == osm_latlon(finish_i[1], model)
+    np = nearby_positions(model[2], model)
+    @test length(np) == 3
+    @test osm_latlon(np[1], model) == (39.5259267, -119.76681090000002)
+
     rand_road = osm_random_road_position(model)
     rand_intersection = random_position(model)
     rand_route = osm_plan_route(rand_road, rand_intersection, model; return_trip = true)
@@ -51,12 +56,23 @@
     @test !osm_is_stationary(model[1])
     move_agent!(model[1], model, 403.2)
     @test length(model[1].route) == 16
+    move_agent!(model[1], model, 1500)
+    @test isempty(model[1].route)
+    @test !osm_is_stationary(model[1])
+    move_agent!(model[1], model, 200)
+    @test !osm_is_stationary(model[1])
+    move_agent!(model[1], model, 200)
+    @test !osm_is_stationary(model[1])
+    move_agent!(model[1], model, 200)
+    @test osm_is_stationary(model[1])
+
     @test osm_is_stationary(model[2])
     @test move_agent!(model[2], model, 50) == nothing
 
     add_agent!(start_r, model, route, finish_r)
     osm_random_route!(model[3], model)
     @test model[3].destination != finish_r
+    move_agent!(model[3], model, 100.0)
 
     for i in 1:5
         s = (start_r[1:2]..., start_r[3] - i)
@@ -65,6 +81,6 @@
     end
 
     @test sort!(nearby_ids(model[6], model, 2)) == [4, 5, 7, 8]
-    @test sort!(nearby_ids(model[6], model, 500)) == [1, 3, 4, 5, 7, 8]
+    @test sort!(nearby_ids(model[6], model, 800.0)) == [3, 4, 5, 7, 8]
 end
 
