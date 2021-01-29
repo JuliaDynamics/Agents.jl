0ae82241c1cd966b247b00d9ff9ad54f2fef195c
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2021-01-29 23:06:00 +0530

    Updated diffusion_limited_aggregation
    * Reordered functions
    * Model properties are now parameters to `initialize_model`
    * Refactored `twist` to `spin` and `wiggle` to `vibration`
    * Updated to use `InteractiveDynamics#0.13.1`, and consequently use `abm_data_exploration`
    * Updated `as` parameter, still needs a bit of trial and error

--- examples/diffusion_limited_aggregation.jl
+++ examples/diffusion_limited_aggregation.jl
@@ -4,13 +4,13 @@ Random.seed!(42)
 @agent Particle ContinuousAgent{2} begin
     radius::Float64
     is_stuck::Bool
-    twist_axis::Array{Float64,1}
+    spin_axis::Array{Float64,1}
 end
 
 Particle(
     id::Int,
     radius::Float64,
-    twist_clockwise::Bool;
+    spin_clockwise::Bool;
     pos = (0.0, 0.0),
     is_stuck = false,
 ) = Particle(
@@ -19,27 +19,59 @@ Particle(
     (0.0, 0.0),
     radius,
     is_stuck,
-    [0.0, 0.0, twist_clockwise ? -1.0 : 1.0],
-)
-
-properties = Dict(
-    :speed => 0.5,   # dt multiplier
-    :wiggle => 0.55,  # position offset
-    :attraction => 0.45,  # absolute attraction value
-    :twist => 0.55,   # absolute tangential velocity
-    :clockwise_fraction => 0.0,
-    :spawn_count => 0,
-    :min_radius => 1.0,
-    :max_radius => 2.0,
+    [0.0, 0.0, spin_clockwise ? -1.0 : 1.0],
 )
 
 rand_circle() = (θ = rand(0.0:0.1:359.9); (cos(θ), sin(θ)))
-particle_radius(min_radius::Float64, max_radius::Float64) = min_radius <= max_radius ?
-rand(min_radius:0.01:max_radius) : min_radius
-function agent_step!(agent::Particle, model)
-    if agent.is_stuck
-        return
+particle_radius(min_radius::Float64, max_radius::Float64) =
+    min_radius <= max_radius ? rand(min_radius:0.01:max_radius) : min_radius
+
+function initialize_model(;
+    initial_particles::Int = 100,
+    space_extents::NTuple{2,Float64} = (150.0, 150.0),
+    speed = 0.5,
+    vibration = 0.55,
+    attraction = 0.45,
+    spin = 0.55,
+    clockwise_fraction = 0.,
+    min_radius = 1.,
+    max_radius = 2.,
+)
+    properties = Dict(
+        :speed => speed,   # dt multiplier
+        :vibration => vibration,  # position offset
+        :attraction => attraction,  # absolute attraction value
+        :spin => spin,   # absolute tangential velocity
+        :clockwise_fraction => clockwise_fraction,
+        :min_radius => min_radius,
+        :max_radius => max_radius,
+        :spawn_count => 0,
+    )
+    space = ContinuousSpace(space_extents, 1.0; periodic = true)
+    model = ABM(Particle, space; properties)
+    center = space_extents ./ 2.0
+    for i = 1:initial_particles
+        particle = Particle(
+            i,
+            particle_radius(min_radius, max_radius),
+            rand() < clockwise_fraction,
+        )
+        add_agent!(particle, model)
     end
+    particle = Particle(
+        initial_particles + 1,
+        particle_radius(min_radius, max_radius),
+        true;
+        pos = center,
+        is_stuck = true,
+    )
+    add_agent_pos!(particle, model)
+    return model
+end
+
+function agent_step!(agent::Particle, model)
+    agent.is_stuck && return
+
     for id in nearby_ids(agent.pos, model, agent.radius)
         if model[id].is_stuck
             agent.is_stuck = true
@@ -49,10 +81,12 @@ function agent_step!(agent::Particle, model)
     end
     radial = model.space.extent ./ 2.0 .- agent.pos
     radial = radial ./ norm(radial)
-    tangent = Tuple(cross([radial..., 0.0], agent.twist_axis)[1:2])
+    tangent = Tuple(cross([radial..., 0.0], agent.spin_axis)[1:2])
     agent.vel =
-        (radial .* model.attraction .+ tangent .* model.twist .+
-        rand_circle() .* model.wiggle) ./ (agent.radius ^ 2.)
+        (
+            radial .* model.attraction .+ tangent .* model.spin .+
+            rand_circle() .* model.vibration
+        ) ./ (agent.radius^2.0)
     move_agent!(agent, model, model.speed)
 end
 
@@ -69,50 +103,23 @@ function model_step!(model)
     end
 end
 
-function initialize_model(;
-    initial_particles::Int = 100,
-    space_extents::NTuple{2,Float64} = (150.0, 150.0),
-    props = properties,
-)
-    space = ContinuousSpace(space_extents, 1.0; periodic = true)
-    model = ABM(Particle, space; properties)
-    center = space_extents ./ 2.0
-    for i = 1:initial_particles
-        particle = Particle(
-            i,
-            particle_radius(model.min_radius, model.max_radius),
-            rand() < model.clockwise_fraction,
-        )
-        add_agent!(particle, model)
-    end
-    particle = Particle(
-        initial_particles + 1,
-        particle_radius(model.min_radius, model.max_radius),
-        true;
-        pos = center,
-        is_stuck = true,
-    )
-    add_agent_pos!(particle, model)
-    return model
-end
-
 model = initialize_model()
 
-using InteractiveChaos, GLMakie
+using InteractiveDynamics, GLMakie
 
 particle_color(a::Particle) = a.is_stuck ? :red : :blue
-particle_size(a::Particle) = 3.4 * a.radius
+particle_size(a::Particle) = 3.8 * a.radius
 params = (
     :attraction => 0.0:0.01:2.0,
     :speed => 0.0:0.01:2.0,
-    :wiggle => 0.0:0.01:2.0,
-    :twist => 0.0:0.01:2.0,
+    :vibration => 0.0:0.01:2.0,
+    :spin => 0.0:0.01:2.0,
     :clockwise_fraction => 0.0:0.01:1.0,
-    :min_radius => 0.5:0.01:3.,
-    :max_radius => 0.5:0.01:3.,
+    :min_radius => 0.5:0.01:3.0,
+    :max_radius => 0.5:0.01:3.0,
 )
 
-interactive_abm(
+abm_data_exploration(
     model,
     agent_step!,
     model_step!,
