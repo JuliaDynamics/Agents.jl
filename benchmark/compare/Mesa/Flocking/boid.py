6e91904bddd1d7d63bb629b78ea8f4dabba4c856
rht <rhtbot@protonmail.com>
2021-01-31 06:44:22 -0500

    Mesa benchmark: Update Flocking code style to be similar to Agents.jl

--- benchmark/compare/Mesa/Flocking/boid.py
+++ benchmark/compare/Mesa/Flocking/boid.py
@@ -56,51 +56,26 @@ class Boid(Agent):
         self.separate_factor = separate
         self.match_factor = match
 
-    def cohere(self, neighbors):
-        """
-        Return the vector toward the center of mass of the local neighbors.
-        """
-        cohere = np.zeros(2)
-        if neighbors:
-            for neighbor in neighbors:
-                cohere += self.model.space.get_heading(self.pos, neighbor.pos)
-            cohere /= len(neighbors)
-        return cohere
-
-    def separate(self, neighbors):
-        """
-        Return a vector away from any neighbors closer than separation dist.
-        """
-        me = self.pos
-        them = (n.pos for n in neighbors)
-        separation_vector = np.zeros(2)
-        for other in them:
-            if self.model.space.get_distance(me, other) < self.separation:
-                separation_vector -= self.model.space.get_heading(me, other)
-        return separation_vector
-
-    def match_heading(self, neighbors):
-        """
-        Return a vector of the neighbors' average heading.
-        """
-        match_vector = np.zeros(2)
-        if neighbors:
-            for neighbor in neighbors:
-                match_vector += neighbor.velocity
-            match_vector /= len(neighbors)
-        return match_vector
-
     def step(self):
         """
         Get the Boid's neighbors, compute the new vector, and move accordingly.
         """
-
         neighbors = self.model.space.get_neighbors(self.pos, self.vision, False)
-        self.velocity += (
-            self.cohere(neighbors) * self.cohere_factor
-            + self.separate(neighbors) * self.separate_factor
-            + self.match_heading(neighbors) * self.match_factor
-        ) / 2
+        N = 0
+        match_vector, separation_vector, cohere = np.zeros((3, 2))
+        for neighbor in neighbors:
+            N += 1
+            heading = self.model.space.get_heading(self.pos, neighbor.pos)
+            cohere += heading
+            if self.model.space.get_distance(self.pos, neighbor.pos) < self.separation:
+                separation_vector -= heading
+            match_vector += neighbor.velocity
+        N = max(N, 1)
+        cohere = cohere / N * self.cohere_factor
+        separation_vector = separation_vector * self.separate_factor
+        match_vector = match_vector / N * self.match_factor
+        #self.velocity = (self.velocity + cohere + separation_vector + match_vector) / 2
+        self.velocity += (cohere + separation_vector + match_vector) / 2
         self.velocity /= np.linalg.norm(self.velocity)
         new_pos = self.pos + self.velocity * self.speed
         self.model.space.move_agent(self, new_pos)
