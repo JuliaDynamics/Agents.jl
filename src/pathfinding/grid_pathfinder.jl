dd5912d707ac0e4e15c89eb9e731f547768c0e58
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2021-02-02 23:42:07 +0530

    A* algorithm now works with periodic grid

--- src/pathfinding/grid_pathfinder.jl
+++ src/pathfinding/grid_pathfinder.jl
@@ -2,9 +2,10 @@ const Path{D} = LinkedList{Dims{D}}
 Path() = nil()
 
 mutable struct Pathfinder{D}
-    agent_paths::Dict{Int, Path{D}}
+    agent_paths::Dict{Int,Path{D}}
     grid_dims::Dims{D}
-    walkable::Array{Bool, D}
+    periodic::Bool
+    walkable::Array{Bool,D}
 end
 
 struct GridCell
@@ -13,53 +14,55 @@ struct GridCell
     h::Int
 end
 
-GridCell(g::Int, h::Int) = GridCell(g+h, g, h)
-sumabs(v) = sum(abs.(v))
+GridCell(g::Int, h::Int) = GridCell(g + h, g, h)
 
-function find_path(
-    pathfinder::Pathfinder{D},
-    from::Dims{D},
-    to::Dims{D},
-) where{D}
+function find_path(pathfinder::Pathfinder{D}, from::Dims{D}, to::Dims{D}) where {D}
     # use a DefaultDict instead?
     grid = fill(GridCell(typemax(Int), typemax(Int), typemax(Int)), pathfinder.grid_dims...)
-    parent = Array{Union{Nothing, Dims{D}}, D}(nothing, pathfinder.grid_dims...)
-    border_dists = [Int(floor(10. * sqrt(x))) for x in 1:D]
-    function dist_cost(delta)
-        delta = collect(abs.(delta))
+    parent = Array{Union{Nothing,Dims{D}},D}(nothing, pathfinder.grid_dims...)
+    border_dists = [Int(floor(10.0 * sqrt(x))) for x = 1:D]
+    function dist_cost(a, b)
+        delta = collect(
+            pathfinder.periodic ?
+            min.(abs.(a .- b), pathfinder.grid_dims .- abs.(a .- b)) : abs.(a .- b),
+        )
         sort!(delta)
         carry = 0
         hdist = 0
-        for i in D:-1:1
+        for i = D:-1:1
             hdist += border_dists[i] * (delta[D+1-i] - carry)
             carry = delta[D+1-i]
         end
         return hdist
     end
 
-    neighbor_offsets = [Tuple(a) for a in Iterators.product([(-1):1 for φ in 1:D]...) if a != zeros(D)]
-    open_list = BinaryMinHeap{Tuple{Int, Dims{D}}}()
+    neighbor_offsets = [
+        Tuple(a)
+        for
+        a in Iterators.product([(-1):1 for φ = 1:D]...) if a != Tuple(zeros(Int, D))
+    ]
+    open_list = BinaryMinHeap{Tuple{Int,Dims{D}}}()
     closed_list = Set{Dims{D}}()
 
-    grid[from...] = GridCell(0, dist_cost(to .- from))
+    grid[from...] = GridCell(0, dist_cost(from, to))
     push!(open_list, (grid[from...].f, from))
 
     while !isempty(open_list)
         _, cur = pop!(open_list)
-
         push!(closed_list, cur)
         cur == to && break
 
         for offset in neighbor_offsets
-            nbor = cur.+offset
+            nbor = cur .+ offset
+            pathfinder.periodic &&
+                (nbor = (nbor .- 1 .+ pathfinder.grid_dims) .% pathfinder.grid_dims .+ 1)
             all(1 .<= nbor .<= pathfinder.grid_dims) || continue
             pathfinder.walkable[nbor...] || continue
             nbor in closed_list && continue
-
-            new_g_cost = grid[cur...].g + dist_cost(offset)
+            new_g_cost = grid[cur...].g + dist_cost(cur, nbor)
             if new_g_cost < grid[nbor...].g
                 parent[nbor...] = cur
-                grid[nbor...] = GridCell(new_g_cost, dist_cost(to.-nbor))
+                grid[nbor...] = GridCell(new_g_cost, dist_cost(nbor, to))
                 # open list will contain duplicates. Can this be avoided?
                 push!(open_list, (grid[nbor...].f, nbor))
             end
