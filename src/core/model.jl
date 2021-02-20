571a39e0f7b1c93bc3b5fbd7ea9d81fdcd819e6d
Tim DuBois <tim@neophilus.net>
2021-02-20 18:05:29 +0100

    additional docs and changelog entry

--- src/core/model.jl
+++ src/core/model.jl
@@ -211,6 +211,8 @@ export iter_agent_groups, map_agent_groups, index_groups_filtered
 Return an iterator over all agents of the model, grouped by order. When `order = 2`, the
 iterator returns agent pairs, e.g `(agent1, agent2)` and when `order = 3`: agent triples,
 e.g. `(agent1, agent7, agent8)`. `order` must be larger than `1` but has no upper bound.
+
+Index order is provided by the [`by_id`](@ref) scheduler.
 """
 iter_agent_groups(order::Int, model::ABM) =
     Iterators.product((map(i -> model[i], by_id(model)) for _ in 1:order)...)
