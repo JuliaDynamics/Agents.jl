e9e3a4bad7af256325eee1eebb03d5acbb27ff88
Xirui Zhao <35286069+xiruizhao@users.noreply.github.com>
2021-02-21 23:42:14 +0800

    Improve docs

--- src/visualization/plot-recipes.jl
+++ src/visualization/plot-recipes.jl
@@ -11,10 +11,10 @@ end
     plotabm(model::ABM{<: ContinuousSpace}; ac, as, am, kwargs...)
     plotabm(model::ABM{<: DiscreteSpace}; ac, as, am, kwargs...)
 
-Plot the `model` as a `scatter`-plot, by configuring the agent shape, color and size
+Plot the `model` as a `scatter`-plot, by configuring the agent color, size and marker (shape)
 via the keywords `ac, as, am`.
 These keywords can be constants, or they can be functions, each accepting an agent
-and outputting a valid value for color/shape/size.
+and outputting a valid value for color/size/marker.
 
 The keyword `scheduler = model.scheduler` decides the plotting order of agents
 (which matters only if there is overlap).
