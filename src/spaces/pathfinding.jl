0de63b1b43a08cc3ab7ef6cff6903cf98a39afcb
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2021-03-04 18:13:27 +0530

    Added docstring for Pathfinder

--- src/spaces/pathfinding.jl
+++ src/spaces/pathfinding.jl
@@ -17,6 +17,26 @@ struct Pathfinder{M<:CostMetric}
     cost_metric::Union{M,Type{M}}
 end
 
+"""
+    Pathfinder(; diagonal_neighbors = true, admissibility = 0.0, walkable = nothing, cost_metric = DirectDistance)
+
+Enable pathfinding using the A* algorithm by passing this struct into [`GridSpace`](@ref). Pathfinding parameters
+can be overriden from their default values using the following keyword arguments:
+
+`diagonal_neighbors` defaults to `true`, and allows agents to move to diagonally adjacent cells. If set to `false`,
+agents are only allowed to move to adjacent neighbors.
+
+`admissibility` allows the algorithm to approximate paths to speed up pathfinding significantly. A value of `ϵ`
+allows paths atmost `(1+ϵ)` times the optimal path length. The default value is `0`, indicating that paths have
+to be optimal.
+
+`walkable` specifies (un)walkable regions of the space. If specified, it should be a boolean array of the same
+dimensions and size as the corresponding [`GridSpace`](@ref). This defaults to `nothing`, which allows agents to walk on any
+position in the space.
+
+`cost_metric` specifies the method to use for approximating the distance between two points. This defaults 
+to [`DirectDistance`](@ref).
+"""
 Pathfinder(;
     diagonal_neighbors::Bool = true,
     admissibility::Float64 = 0.0,
