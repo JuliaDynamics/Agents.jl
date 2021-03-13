a4e473b7111852ac33ab91e6977e4b020a40b5a1
Datseris <datseris.george@gmail.com>
2021-03-13 10:23:27 +0100

    Improve documentation of Pathfinder
    @AayushSabharwal please have a look at my changes. Documentation should be harmonious in the way it works. Further more, it is better to state default values with equals (`diagonal_movement = true`) instead of saying their default values by English words.
    
    The syntax you use, that allows users to pass both DistanceMetric, and DistanceMetric(), is a bad idea. In Julia these are two different things, and should be treated as such. The "convenience" of not having to type `()` is a null argument. It makes no sense. Let's keep things and enforce that input arguments are what they should be.

--- src/spaces/pathfinding.jl
+++ src/spaces/pathfinding.jl
@@ -10,37 +10,40 @@ between two points in a `D` dimensional [`GridSpace{D}`](@ref).
 """
 abstract type CostMetric{D} end
 
-struct Pathfinder{M<:CostMetric}
+struct Pathfinder{W<:Union{Array{Bool},Nothing}, M<:CostMetric}
     diagonal_movement::Bool
     admissibility::Float64
-    walkable::Union{Array{Bool},Nothing}
-    cost_metric::Union{M,Type{M}}
+    walkable::W
+    cost_metric::M
 end
 
 """
     Pathfinder(; kwargs...)
 
 Enable pathfinding using the A* algorithm by passing an instance of `Pathfinder` into
-[`GridSpace`](@ref). Pathfinding parameters are tuned with the following keyword arguments:
+[`GridSpace`](@ref). Pathfinding works by using the functions
+[`set_target!`](@ref) and [`move_along_route`](@ref) see [Path-finding](@ref) for more.
 
-* `diagonal_movement = true` states that agents are allowed to move diagonally. and allows agents to move to diagonally adjacent cells. If set to `false`,
-agents are only allowed to move to adjacent neighbors.
+## Keywords
 
-`admissibility` allows the algorithm to approximate paths to speed up pathfinding significantly. A value of `ϵ`
-allows paths atmost `(1+ϵ)` times the optimal path length. The default value is `0`, indicating that paths have
-to be optimal.
-
-`walkable` specifies (un)walkable regions of the space. If specified, it should be a boolean array of the same
-dimensions and size as the corresponding [`GridSpace`](@ref). This defaults to `nothing`, which allows agents to walk on any
-position in the space.
-
-`cost_metric` specifies the method to use for approximating the distance between two points. This defaults
-to [`DirectDistance`](@ref).
+* `diagonal_movement = true` states that agents are allowed to move diagonally.
+  Otherwise, only orthogonal directions are possible.
+* `admissibility = 0` allows the algorithm to approximate paths to speed up pathfinding
+  significantly. A value of `admissibility` allows paths atmost `(1+admissibility)` times
+  the optimal path length.
+* `walkable = nothing` specifies (un)walkable regions of the space. If specified, it should
+  be a boolean array of the same size as the corresponding
+  [`GridSpace`](@ref). This defaults to `nothing`, which allows agents to walk on any
+  position in the space.
+* `cost_metric = DirectDistance()` is an instance of a cost metric and specifies the method
+  to use for approximating the distance between two points. This defaults
+  to [`DirectDistance`](@ref).
 """
-Pathfinder(;
+function Pathfinder(;
     diagonal_movement::Bool = true,
     admissibility::Float64 = 0.0,
     walkable::Union{Array{Bool},Nothing} = nothing,
-    cost_metric::Union{Type{M},M} = DirectDistance,
-) where {M<:CostMetric} =
-    Pathfinder(diagonal_movement, admissibility, walkable, cost_metric)
+    cost_metric::CostMetric = DirectDistance(),
+)
+    return Pathfinder(diagonal_movement, admissibility, walkable, cost_metric)
+end
