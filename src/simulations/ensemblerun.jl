21142cc0c26eb7f1858a5fc3a376e144e209aeb3
Frederik Banning <65158285+fbanning@users.noreply.github.com>
2021-04-07 13:00:10 +0200

    fix typo

--- src/simulations/ensemblerun.jl
+++ src/simulations/ensemblerun.jl
@@ -37,7 +37,7 @@ end
 
 """
     ensemblerun!(generator, agent_step!, model_step!, n; kwargs...)
-Generate many `ABM`s and propagate them into `ensemblerun!(models, ...)` using the
+Generate many `ABM`s and propagate them into `ensemblerun!(models, ...)` using
 the provided `generator` which is a one-argument function whose input is a seed.
 
 This method has additional keywords `ensemble = 5, seeds = rand(UInt32, ensemble)`.
