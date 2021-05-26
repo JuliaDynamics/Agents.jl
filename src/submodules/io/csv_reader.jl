7e27c319c2dcad52bd7c4d55f37b75e67eda559f
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2021-05-26 18:24:46 +0530

    Docstring, additional check
    * Docstring for populate_from_csv!
    * Check if `agent_type` is a function before trying to get its fields

--- src/submodules/io/csv_reader.jl
+++ src/submodules/io/csv_reader.jl
@@ -1,12 +1,22 @@
 using CSV
 
+"""
+    populate_from_csv!(filename, model, agent_type; kwargs...)
+
+Populates the given `model` using data read from csv file at `filename`.
+`agent_type` is either a subtype of `AbstractAgent` also used in `model` or
+a function that returns an agent that can be added to the model. All keyword arguments
+are forwarded to `CSV.Rows`. If `agent_type` is not a function, and the `types` keyword
+argument is not specified, it is assumed that the columns in the csv file correspond
+to fields of the struct.
+"""
 function populate_from_csv!(
     filename,
     model::ABM{A},
     agent_type::B;
     kwargs...
 ) where {A, B<:Union{Type{<:A}, Function}}
-    !haskey(kwargs, :types) && (kwargs[:types] = collect(fieldtypes(agent_type)))
+    !(agent_type isa Function) && !haskey(kwargs, :types) && (kwargs[:types] = collect(fieldtypes(agent_type)))
     for row in Rows(filename; kwargs...)
         add_agent_pos!(agent_type(row...), model)
     end
