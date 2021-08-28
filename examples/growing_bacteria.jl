a1df207479ea0b838a16823be9e5d66c48779ed2
michielstock <michielfmstock@gmail.com>
2021-08-28 17:54:55 +0200

    :wrench: `walk!` instead of `move_agent!`

--- examples/growing_bacteria.jl
+++ examples/growing_bacteria.jl
@@ -69,8 +69,8 @@ function model_step!(model)
         if a.growthprog ≥ 1
             ## When a cell has matured, it divides into two daughter cells on the
             ## positions of its nodes.
-            add_agent!(a.p1, model, 0.0, a.orientation, 0.0, 0.1 * rand(model.rng) + 0.05)
-            add_agent!(a.p2, model, 0.0, a.orientation, 0.0, 0.1 * rand(model.rng) + 0.05)
+            add_agent!(a.pos, model, 0.0, a.orientation, 0.0, 0.1 * rand(model.rng) + 0.05)
+            add_agent!(a.pos, model, 0.0, a.orientation, 0.0, 0.1 * rand(model.rng) + 0.05)
             kill_agent!(a, model)
         else
             ## The rest lengh of the internal spring grows with time. This causes
@@ -96,8 +96,8 @@ nothing # hide
 # valid for small length scales, where viscous forces dominate over inertia.
 function agent_step!(agent::SimpleCell, model::ABM)
     fsym, compression, torque = transform_forces(agent)
-    new_pos = agent.pos .+ model.dt * model.mobility .* fsym
-    move_agent!(agent, new_pos, model)
+    direction = model.dt * model.mobility .* fsym
+    walk!(agent, direction, model)
     agent.length += model.dt * model.mobility .* compression
     agent.orientation += model.dt * model.mobility .* torque
     agent.growthprog += model.dt * agent.growthrate
