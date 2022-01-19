67f87e848df8f8922cc3b711fb070434f366dcd7
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2022-01-19 16:23:01 +0530

    Update `test_map` doc, Fix zombies example (#588)
    * Update `test_map` doc, Fix zombies example
    
    * Use :distance as default, update docs
    
    * Update `:time` docs, zombies use speed, tests

--- src/models/zombies.jl
+++ src/models/zombies.jl
@@ -2,6 +2,7 @@ using Random
 
 @agent Zombie OSMAgent begin
     infected::Bool
+    speed::Float64
 end
 
 """
@@ -10,18 +11,27 @@ Same as in the [Zombie Outbreak](@ref) example.
 """
 function zombies(; seed = 1234)
     map_path = OSM.test_map()
-    model = ABM(Zombie, OpenStreetMapSpace(map_path); rng = Random.MersenneTwister(seed))
+    properties = Dict(:dt => 1 / 60)
+    model = ABM(
+        Zombie,
+        OpenStreetMapSpace(map_path);
+        properties = properties,
+        rng = Random.MersenneTwister(seed)
+    )
 
     for id in 1:100
         start = random_position(model) # At an intersection
-        human = Zombie(id, start, false)
+        speed = rand(model.rng) * 5.0 + 2.0 # Random speed from 2-7kmph
+        human = Zombie(id, start, false, speed)
         add_agent_pos!(human, model)
         OSM.random_route!(human, model; limit = 50) # try 50 times to find a random route
     end
     ## We'll add patient zero at a specific (latitude, longitude)
     start = OSM.road((51.5328328, 9.9351811), model)
     finish = OSM.intersection((51.530876112711745, 9.945125635913511), model)
-    zombie = add_agent!(start, model, true)
+
+    speed = rand(model.rng) * 5.0 + 2.0 # Random speed from 2-7kmph
+    zombie = add_agent!(start, model, true, speed)
     plan_route!(zombie, finish, model)
     ## This function call creates & adds an agent, see `add_agent!`
     return model, zombie_agent_step!, dummystep
@@ -29,18 +39,18 @@ end
 
 function zombie_agent_step!(agent, model)
     ## Each agent will progress slightly along their route
-    move_along_route!(agent, model, 0.002)
+    move_along_route!(agent, model, agent.speed * model.dt)
 
     if is_stationary(agent, model) && rand(model.rng) < 0.1
         ## When stationary, give the agent a 10% chance of going somewhere else
         OSM.random_route!(agent, model; limit = 50)
         ## Start on new route
-        move_along_route!(agent, model, 0.0001)
+        move_along_route!(agent, model, agent.speed * model.dt)
     end
 
     if agent.infected
-        ## Agents will be infected if they get too close to a zombie.
-        map(i -> model[i].infected = true, nearby_ids(agent, model, 0.0003))
+        ## Agents will be infected if they get too close (within 10m) to a zombie.
+        map(i -> model[i].infected = true, nearby_ids(agent, model, 0.01))
     end
     return
 end
--- src/spaces/openstreetmap.jl
+++ src/spaces/openstreetmap.jl
@@ -43,6 +43,16 @@ Create a space residing on the Open Street Map (OSM) file provided via `path`.
 A sample file is provided using [`OSM.test_map`](@ref). Additional maps can
 be downloaded using the [functions provided by LightOSM.jl](https://deloittedigitalapac.github.io/LightOSM.jl/docs/download_network/).
 The functionality related to Open Street Map spaces is in the submodule `OSM`.
+Agents.jl also re-exports [`OSM.download_osm_network`](@ref). An example usage
+to download the map of London to "london.json":
+
+```julia
+OSM.download_osm_network(
+    :place_name;
+    place_name = "London",
+    save_to_file_location = "london.json"
+)
+```
 
 This space represents the underlying map as a *continuous* entity choosing accuracy over
 performance. The map is represented as a graph, consisting of nodes connected by edges. Nodes
@@ -54,8 +64,10 @@ map's `weight_type` as listed in the documentation for
 [`LightOSM.OSMGraph`](https://deloittedigitalapac.github.io/LightOSM.jl/docs/types/#LightOSM.OSMGraph).
 The possible `weight_type`s are:
 - `:distance`: The distance in kilometers of an edge
-- `:time`: The time in hours to travel along an edge
+- `:time`: The time in hours to travel along an edge at the maximum speed allowed on that road
 - `:lane_efficiency`: Time scaled by number of lanes
+
+The default `weight_type` used is `:distance`.
 
 An example of its usage can be found in [Zombie Outbreak](@ref).
 
@@ -100,7 +112,7 @@ function OpenStreetMapSpace(
     path::AbstractString;
     kwargs...
 )
-    m = graph_from_file(path; kwargs...)
+    m = graph_from_file(path; weight_type = :distance, kwargs...)
     agent_positions = [Int[] for _ in 1:Agents.nv(m.graph)]
     return OpenStreetMapSpace(m, agent_positions, Dict())
 end
@@ -120,7 +132,7 @@ Download a small test map of [Göttingen](https://www.openstreetmap.org/export#m
 as an artifact. Return a path to the downloaded file.
 
 Using this map requires `network_type = :none` to be passed as a keyword
-to [`OSMSpace`](@ref).
+to [`OSMSpace`](@ref). The unit of distance used for this map is `:time`.
 """
 test_map() = joinpath(artifact"osm_map_gottingen", "osm_map_gottingen.json")
 
