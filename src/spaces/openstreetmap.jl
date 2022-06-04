1818f2ed247060f2cf185c05416f9bd2e1245740
Datseris <datseris.george@gmail.com>
2022-06-04 16:03:52 +0200

    fix incorrect usage of `_`

--- src/spaces/openstreetmap.jl
+++ src/spaces/openstreetmap.jl
@@ -741,7 +741,7 @@ end
     move_along_route!(agent, model::ABM{<:OpenStreetMapSpace}, distance::Real) → remaining
 
 Move an agent by `distance` along its planned route. Units of distance are as specified
-by the underlying graph's weight_type. If the provided `distance` is greater than the
+by the underlying graph's `weight_type`. If the provided `distance` is greater than the
 distance to the end of the route, return the remaining distance. Otherwise, return `0`.
 `0` is also returned if `is_stationary(agent, model)`.
 """
