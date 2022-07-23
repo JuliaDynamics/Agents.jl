e17b4ea61b47e821165299bd5777fb3a29299f92
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2022-07-23 23:40:10 +0530

    Documentation update

--- src/spaces/utilities.jl
+++ src/spaces/utilities.jl
@@ -157,6 +157,8 @@ end
     normalize_position(pos, model::ABM{<:Union{AbstractGridSpace,ContinuousSpace}})
 
 Return the position `pos` normalized for the extents of the space of the given `model`.
+For periodic spaces, this wraps the position along each dimension, while for non-periodic
+spaces this clamps the position to the space extent.
 """
 normalize_position(pos, model::ABM) = normalize_position(pos, model.space)
 
