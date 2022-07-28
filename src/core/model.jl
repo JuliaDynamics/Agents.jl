2cc09f3c23f2d4195af70f9ed72daacb202009d2
Datseris <datseris.george@gmail.com>
2022-07-28 19:41:23 +0200

    use model scheduler in `iter_agent_groups`

--- src/core/model.jl
+++ src/core/model.jl
@@ -244,10 +244,10 @@ Return an iterator over all agents of the model, grouped by order. When `order =
 iterator returns agent pairs, e.g `(agent1, agent2)` and when `order = 3`: agent triples,
 e.g. `(agent1, agent7, agent8)`. `order` must be larger than `1` but has no upper bound.
 
-Index order is provided by the [`Schedulers.by_id`](@ref) scheduler by default, but can be altered
-with the `scheduler` keyword.
+Index order is provided by the model scheduler by default,
+but can be altered with the `scheduler` keyword.
 """
-iter_agent_groups(order::Int, model::ABM; scheduler = Schedulers.by_id) =
+iter_agent_groups(order::Int, model::ABM; scheduler = model.scheduler) =
     Iterators.product((map(i -> model[i], scheduler(model)) for _ in 1:order)...)
 
 """
