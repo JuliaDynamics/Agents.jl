be647b8d5ab9ad2c1b5ca91bcacfcfdb80a72366
Interdisciplinary Physics Team (InPhyT) <inphyt@gmail.com>
2022-10-26 13:03:31 +0200

    Rename add/rem_node! to Graphs.add/rem_vertex!, implement rem_edge! (#693)
    * Rename add/rem_node! to Graphs.add/rem_vertex!, implement rem_edge!
    
    Co-Authored-By: Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
    Co-Authored-By: Claudio Moroni <43729990+ClaudMor@users.noreply.github.com>
    
    * Rename add/rem_node! to Graphs.add/rem_vertex!, implement rem_edge!
    
    - Added deprecations in deprecations.jl file
    - Updated Project.toml minor version from 5.5 to 5.6
    - Added relevant CHANGELOG entry
    - Changed the names in the documentation and relevant docstrings
    - Added the new method `rem_edge!` to the documentation
    
    Co-Authored-By: Claudio Moroni <43729990+ClaudMor@users.noreply.github.com>
    Co-Authored-By: Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
    
    Co-authored-by: Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
    Co-authored-by: Claudio Moroni <43729990+ClaudMor@users.noreply.github.com>

--- src/spaces/graph.jl
+++ src/spaces/graph.jl
@@ -19,7 +19,7 @@ The position type for this space is `Int`, use [`GraphAgent`](@ref) for convenie
 
 `Graphs.nv` and `Graphs.ne` can be used in a model with a `GraphSpace` to obtain
 the number of nodes or edges in the graph.
-The underlying graph can be altered using [`add_node!`](@ref) and [`rem_node!`](@ref).
+The underlying graph can be altered using [`add_vertex!`](@ref) and [`rem_vertex!`](@ref).
 
 An example using `GraphSpace` is [SIR model for the spread of COVID-19](@ref).
 
@@ -126,18 +126,49 @@ end
 #######################################################################################
 # Mutable graph functions
 #######################################################################################
-export rem_node!, add_node!, add_edge!
+export rem_node!, add_node!, rem_vertex!, add_vertex!, add_edge!, rem_edge!
 
 """
-    rem_node!(model::ABM{<: GraphSpace}, n::Int)
+     rem_node!(model::ABM{<: GraphSpace}, n::Int)
+Remove node (i.e. position) `n` from the model's graph. All agents in that node are killed.
+**Warning:** Graphs.jl (and thus Agents.jl) swaps the index of the last node with
+that of the one to be removed, while every other node remains as is. This means that
+ when doing `rem_node!(n, model)` the last node becomes the `n`-th node while the previous
+ `n`-th node (and all its edges and agents) are deleted.
+ """
+ function rem_node!(model::ABM{<:GraphSpace}, n::Int)
+     for id in copy(ids_in_position(n, model))
+         kill_agent!(model[id], model)
+     end
+    V = nv(model)
+    success = Graphs.rem_vertex!(model.space.graph, n)
+    n > V && error("Node number exceeds amount of nodes in graph!")
+    s = model.space.stored_ids
+    s[V], s[n] = s[n], s[V]
+    pop!(s)
+end
+
+"""
+    add_node!(model::ABM{<: GraphSpace})
+ Add a new node (i.e. possible position) to the model's graph and return it.
+ You can connect this new node with existing ones using [`add_edge!`](@ref).
+ """
+ function add_node!(model::ABM{<:GraphSpace})
+     add_vertex!(model.space.graph)
+     push!(model.space.stored_ids, Int[])
+     return nv(model)
+ end
+
+"""
+    rem_vertex!(model::ABM{<:GraphSpace}, n::Int)
 Remove node (i.e. position) `n` from the model's graph. All agents in that node are killed.
 
 **Warning:** Graphs.jl (and thus Agents.jl) swaps the index of the last node with
 that of the one to be removed, while every other node remains as is. This means that
-when doing `rem_node!(n, model)` the last node becomes the `n`-th node while the previous
+when doing `rem_vertex!(n, model)` the last node becomes the `n`-th node while the previous
 `n`-th node (and all its edges and agents) are deleted.
 """
-function rem_node!(model::ABM{<:GraphSpace}, n::Int)
+function Graphs.rem_vertex!(model::ABM{<:GraphSpace}, n::Int)
     for id in copy(ids_in_position(n, model))
         kill_agent!(model[id], model)
     end
@@ -150,19 +181,28 @@ function rem_node!(model::ABM{<:GraphSpace}, n::Int)
 end
 
 """
-    add_node!(model::ABM{<: GraphSpace})
+    add_vertex!(model::ABM{<:GraphSpace})
 Add a new node (i.e. possible position) to the model's graph and return it.
 You can connect this new node with existing ones using [`add_edge!`](@ref).
 """
-function add_node!(model::ABM{<:GraphSpace})
+function Graphs.add_vertex!(model::ABM{<:GraphSpace})
     add_vertex!(model.space.graph)
     push!(model.space.stored_ids, Int[])
     return nv(model)
 end
 
 """
-    add_edge!(model::ABM{<: GraphSpace}, n::Int, m::Int)
+    add_edge!(model::ABM{<:GraphSpace},  args...; kwargs...)
 Add a new edge (relationship between two positions) to the graph.
-Returns a boolean, true if the operation was succesful.
+Returns a boolean, true if the operation was successful. 
+
+`args` and `kwargs` are directly passed to the `add_edge!` dispatch that acts the underlying graph type.
 """
-Graphs.add_edge!(model::ABM{<:GraphSpace}, n, m) = add_edge!(model.space.graph, n, m)
+Graphs.add_edge!(model::ABM{<:GraphSpace}, args...; kwargs...) = add_edge!(model.space.graph, args...; kwargs...)
+
+"""
+    rem_edge!(model::ABM{<:GraphSpace}, n, m)
+Remove an edge (relationship between two positions) from the graph.
+Returns a boolean, true if the operation was successful. 
+"""
+Graphs.rem_edge!(model::ABM{<:GraphSpace}, n, m) = rem_edge!(model.space.graph, n, m)
--- test/graph_tests.jl
+++ test/graph_tests.jl
@@ -11,14 +11,14 @@
     ids_in_position(1, abm) == 1:5
     ids_in_position(2, abm) == 6:10
 
-    rem_node!(abm, 2)
+    rem_vertex!(abm, 2)
     @test nv(abm) == 4
     @test nagents(abm) == 20
     # Last node became 2nd node (swapped places as per Graphs.jl)
     @test ids_in_position(2, abm) == 21:25
     @test nv(abm) == 4
 
-    n = add_node!(abm)
+    n = add_vertex!(abm)
     @test n == 5
     @test nv(abm) == 5
 
@@ -27,12 +27,17 @@
     ids = nearby_ids(a, abm)
     @test sort(ids) == 21:25
 
+
+    rem_edge!(abm, n, 2)
+    ids = nearby_ids(a, abm)
+    @test isempty(ids)
+
     abm = ABM(Agent5, GraphSpace(SimpleGraph()))
     @test_throws ArgumentError add_agent!(abm, rand(abm.rng))
-    add_node!(abm)
+    add_vertex!(abm)
     @test nv(abm) == 1
     @test add_edge!(abm, 1, 2) == false
-    add_node!(abm)
+    add_vertex!(abm)
     @test add_edge!(abm, 1, 2) == true
 
 end
