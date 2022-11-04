e2178907bc38727c26779231930756dcfeb6d982
Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
2022-11-04 11:09:06 +0100

    Fix typos in CHANGELOG and tests (#696)
    * Fix a typo in docstring
    
    * Fix a few typos in the docstrings
    
    * Fix a typo
    
    * Create FUNDING.yml
    
    * Delete FUNDING.yml
    
    * Clean the examples
    
    Fixed a few typos.
    
    * Fix typos in CHANGELOG and tests

--- examples/flock.jl
+++ examples/flock.jl
@@ -35,12 +35,12 @@ end
 # using [`ContinuousAgent`](@ref) is required for using [`move_agent!`](@ref)
 # in `ContinuousSpace` with a time-stepping method.
 # `speed` defines how far the bird travels in the direction defined by `vel` per `step`.
-# `seperation` defines the minimum distance a bird must maintain from its neighbors.
+# `separation` defines the minimum distance a bird must maintain from its neighbors.
 # `visual_distance` refers to the distance a bird can see and defines a radius of neighboring birds.
-# The contribution of each rule defined above recieves an importance weight: `cohere_factor`
+# The contribution of each rule defined above receives an importance weight: `cohere_factor`
 # is the importance of maintaining the average position of neighbors,
 # `match_factor` is the importance of matching the average trajectory of neighboring birds,
-# and `separate_factor` is the importance of maining the minimum
+# and `separate_factor` is the importance of maintaining the minimum
 # distance from neighboring birds.
 
 # The function `initialize_model` generates birds and returns a model object using default values.
