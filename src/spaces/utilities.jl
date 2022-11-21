b9db553f637f875acac6f2048688691e1f355e08
Riccardo Foffi <riccardo.foffi@protonmail.com>
2022-11-21 19:41:55 +0100

    define correct azimuthal angle distribution

--- src/spaces/utilities.jl
+++ src/spaces/utilities.jl
@@ -274,12 +274,25 @@ function randomwalk!(
     walk!(agent, direction, model)
 end
 
+# define new distribution to obtain spherically uniform rotations in 3D
+struct Arccos{T<:Real} <: ContinuousUnivariateDistribution
+    a::T
+    b::T
+    Arccos{T}(a::T,b::T) where {T} = new{T}(a::T,b::T)
+end
+function Arccos(a::Real, b::Real; check_args::Bool=true)
+    Distributions.@check_args Arccos a≤b -1≤a≤1 -1≤b≤1
+    return Arccos{Float64}(Float64(a), Float64(b))
+end
+Arccos() = Arccos(-1,1)
+Base.rand(rng::AbstractRNG, d::Arccos) = acos(rand(rng, Uniform(d.a, d.b)))
+
 function randomwalk!(
     agent::AbstractAgent,
     model::ABM{<:AbstractContinuousSpace{3}},
     r::Real;
     polar::ContinuousUnivariateDistribution=Uniform(-π,π),
-    azimuthal::ContinuousUnivariateDistribution=Uniform(-π,π),
+    azimuthal::ContinuousUnivariateDistribution=Arccos(-1,1),
 )
     θ = rand(polar)
     ϕ = rand(azimuthal)
