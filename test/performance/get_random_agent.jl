24b1802868808a51db9305684ac2f1aac5df6f33
jacobusmmsmit <jacobusmmsmit@gmail.com>
2023-02-17 13:26:06 +0000

    Properly escape benchmarks

--- test/performance/get_random_agent.jl
+++ test/performance/get_random_agent.jl
@@ -7,7 +7,7 @@ mutable struct LabelledAgent <: AbstractAgent
 end
 
 n_agents = 100
-agents = [LabelledAgent(id, id<=n_agents/100) for id in 1:n_agents]
+agents = [LabelledAgent(id, id<=n_agents/3) for id in 1:n_agents]
 noremove_model = UnkillableABM(LabelledAgent)
 dict_model = ABM(LabelledAgent)
 fixed_model = FixedMassABM(agents)
@@ -32,19 +32,19 @@ end
 
 # All times are median
 # FixedMassABM
-@benchmark optimistic_random_agent(fixed_model, cond)
-@benchmark allocating_random_agent(fixed_model, cond)
-@benchmark random_agent(fixed_model, cond)
-@benchmark old_random_agent(fixed_model, cond)
+@benchmark optimistic_random_agent($fixed_model, $cond)
+@benchmark allocating_random_agent($fixed_model, $cond)
+@benchmark random_agent($fixed_model, $cond)
+@benchmark old_random_agent($fixed_model, $cond)
 
 # UnkillableABM
-@benchmark optimistic_random_agent(noremove_model, cond)
-@benchmark allocating_random_agent(noremove_model, cond)
-@benchmark random_agent(noremove_model, cond)
-@benchmark old_random_agent(noremove_model, cond)
+@benchmark optimistic_random_agent($noremove_model, $cond)
+@benchmark allocating_random_agent($noremove_model, $cond)
+@benchmark random_agent($noremove_model, $cond)
+@benchmark old_random_agent($noremove_model, $cond)
 
 # DictionaryABM
-@benchmark optimistic_random_agent(dict_model, cond)
-@benchmark allocating_random_agent(dict_model, cond)
-@benchmark random_agent(dict_model, cond)
-@benchmark old_random_agent(dict_model, cond)
+@benchmark optimistic_random_agent($dict_model, $cond)
+@benchmark allocating_random_agent($dict_model, $cond)
+@benchmark random_agent($dict_model, $cond)
+@benchmark old_random_agent($dict_model, $cond)
