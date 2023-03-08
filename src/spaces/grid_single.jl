788e1f71761129c5d9829d739f0020747c2fd7c5
Tortar <68152031+Tortar@users.noreply.github.com>
2023-03-08 12:01:31 +0100

    Check if position is already occupied (#758)
    * Update grid_single.jl
    
    * Update grid_single.jl
    
    * improve error message
    
    * Change tense in error message to past
    
    * Update src/spaces/grid_single.jl
    
    Co-authored-by: Martin Smit <57063134+jacobusmmsmit@users.noreply.github.com>
    
    * add test
    
    ---------
    
    Co-authored-by: Martin Smit <57063134+jacobusmmsmit@users.noreply.github.com>

--- src/spaces/grid_single.jl
+++ src/spaces/grid_single.jl
@@ -37,9 +37,12 @@ function GridSpaceSingle(d::NTuple{D,Int}; periodic = true, metric = :chebyshev)
 end
 # Implementation of space API
 function add_agent_to_space!(a::A, model::ABM{<:GridSpaceSingle,A}) where {A<:AbstractAgent}
-    model.space.stored_ids[a.pos...] = a.id
+    pos = a.pos
+    !isempty(pos, model) && error("Cannot add agent $(a) to occupied position $(pos)")
+    model.space.stored_ids[pos...] = a.id
     return a
 end
+
 function remove_agent_from_space!(a::A, model::ABM{<:GridSpaceSingle,A}) where {A<:AbstractAgent}
     model.space.stored_ids[a.pos...] = 0
     return a
