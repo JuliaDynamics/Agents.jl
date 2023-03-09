2ea619446cb5506f0c70510b4e4d5182a7f8e243
George Datseris <datseris.george@gmail.com>
2023-03-09 15:51:49 +0000

    Update src/spaces/graph.jl
    Co-authored-by: Martin Smit <57063134+jacobusmmsmit@users.noreply.github.com>

--- src/spaces/graph.jl
+++ src/spaces/graph.jl
@@ -161,7 +161,7 @@ end
 
 """
     rem_vertex!(model::ABM{<:GraphSpace}, n::Int)
-Remove node (i.e. position) `n` from the model's graph. All agents in that node are removed.
+Remove node (i.e. position) `n` from the model's graph. All agents in that node are removed from the model.
 
 **Warning:** Graphs.jl (and thus Agents.jl) swaps the index of the last node with
 that of the one to be removed, while every other node remains as is. This means that
