71ab08915102d74c6b4d1c9d35f7ff82a8ea494e
Tortar <68152031+Tortar@users.noreply.github.com>
2023-03-14 02:39:20 +0100

    Update collect.jl

--- src/simulations/collect.jl
+++ src/simulations/collect.jl
@@ -219,7 +219,7 @@ function single_agent_types!(
     model::ABM,
     properties::AbstractArray,
 )
-    a = model[first(allids(model))]
+    a = first(allagents(model))
     for (i, k) in enumerate(properties)
         current_type = typeof(get_data(a, k, identity))
         isconcretetype(current_type) || @warn(
