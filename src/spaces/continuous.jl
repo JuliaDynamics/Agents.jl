63ad5a4771c472e2344d09780a2cc0125e70be6e
Tortar <68152031+Tortar@users.noreply.github.com>
2023-05-14 21:05:31 +0200

    add comment about counter

--- src/spaces/continuous.jl
+++ src/spaces/continuous.jl
@@ -448,6 +448,7 @@ function true_pairs!(pairs::Vector{Tuple{Int,Int}}, model::ABM{<:ContinuousSpace
         end
     end
     to_remove = Int[]
+    # `counter` counts the number of occurencies for each item, it comes from DataStructure.jl
     for doubles in [k for (k,v) in counter(Iterators.flatten(pairs)) if v>1]
         # This list is the set of pairs that have two distances in the pair list.
         # The one with the largest distance value must be dropped.
