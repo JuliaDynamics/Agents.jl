24365de14226b599bfbb32df2b20c4e03ee6d06b
Datseris <datseris.george@gmail.com>
2023-06-01 15:29:24 +0100

    extend convenience

--- src/visualizations.jl
+++ src/visualizations.jl
@@ -134,3 +134,62 @@ struct ABMObservable{M, AS, MS, AD, MD, ADF, MDF, W, S}
     when::W
 end
 export ABMObservable
+
+"""
+    abmexploration(model::ABM; alabels, mlabels, kwargs...)
+
+Open an interactive application for exploring an agent based model and
+the impact of changing parameters on the time evolution. Requires `Agents`.
+
+The application evolves an ABM interactively and plots its evolution, while allowing
+changing any of the model parameters interactively and also showing the evolution of
+collected data over time (if any are asked for, see below).
+The agent based model is plotted and animated exactly as in [`abmplot`](@ref),
+and the `model` argument as well as splatted `kwargs` are propagated there as-is.
+This convencience function *only works for aggregated agent data*.
+
+Calling `abmexploration` returns: `fig::Figure, abmobs::ABMObservable`. So you can save
+and/or further modify the figure and it is also possible to access the collected data
+(if any) via the `ABMObservable`.
+
+Clicking the "reset" button will add a red vertical line to the data plots for visual
+guidance.
+
+## Keywords arguments (in addition to those in `abmplot`)
+* `alabels, mlabels`: If data are collected from agents or the model with `adata, mdata`,
+  the corresponding plots' y-labels are automatically named after the collected data.
+  It is also possible to provide `alabels, mlabels` (vectors of strings with exactly same
+  length as `adata, mdata`), and these labels will be used instead.
+* `figure = NamedTuple()`: Keywords to customize the created Figure.
+* `axis = NamedTuple()`: Keywords to customize the created Axis.
+* `plotkwargs = NamedTuple()`: Keywords to customize the styling of the resulting
+  [`scatterlines`](https://makie.juliaplots.org/dev/examples/plotting_functions/scatterlines/index.html) plots.
+"""
+function abmexploration end
+export abmexploration
+
+"""
+    abmvideo(file, model, agent_step! [, model_step!]; kwargs...)
+
+This function exports the animated time evolution of an agent based model into a video
+saved at given path `file`, by recording the behavior of the interactive version of
+[`abmplot`](@ref) (without sliders).
+The plotting is identical as in [`abmplot`](@ref) and applicable keywords are propagated.
+
+## Keywords
+* `spf = 1`: Steps-per-frame, i.e. how many times to step the model before recording a new
+  frame.
+* `framerate = 30`: The frame rate of the exported video.
+* `frames = 300`: How many frames to record in total, including the starting frame.
+* `title = ""`: The title of the figure.
+* `showstep = true`: If current step should be shown in title.
+* `figure = NamedTuple()`: Figure related keywords (e.g. resolution, backgroundcolor).
+* `axis = NamedTuple()`: Axis related keywords (e.g. aspect).
+* `recordkwargs = NamedTuple()`: Keyword arguments given to `Makie.record`.
+  You can use `(compression = 1, profile = "high")` for a higher quality output,
+  and prefer the `CairoMakie` backend.
+  (compression 0 results in videos that are not playable by some software)
+* `kwargs...`: All other keywords are propagated to [`abmplot`](@ref).
+"""
+function abmvideo end
+export abmvide
