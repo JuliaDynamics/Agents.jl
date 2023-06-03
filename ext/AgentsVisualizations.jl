373c5d6f464d3316b0cf5e047a3127b07317bc6d
Datseris <datseris.george@gmail.com>
2023-06-03 23:31:02 +0100

    define JuliaDynamics colors

--- ext/AgentsVisualizations.jl
+++ ext/AgentsVisualizations.jl
@@ -2,6 +2,14 @@ module AgentsVisualizations
 
 using Agents, Makie
 using Agents: AbstractGridSpace
+JULIADYNAMICS_COLORS = [
+    "#7143E0",
+    "#191E44",
+    "#0A9A84",
+    "#AF9327",
+    "#791457",
+    "#6C768C",
+]
 
 include("src/abmplot.jl")
 include("src/lifting.jl")
