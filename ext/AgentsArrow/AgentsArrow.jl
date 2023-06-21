3971829445ffafea89af163ada3d775cb5eee407
Frederik Banning <frederik.banning@ruhr-uni-bochum.de>
2023-06-21 18:31:20 +0200

    Merge AgentsIO placeholder funcs into AgentsArrow

--- ext/AgentsArrow/AgentsArrow.jl
+++ ext/AgentsArrow/AgentsArrow.jl
@@ -10,4 +10,16 @@ function Agents.writer_arrow(filename, data, append)
     end
 end
 
+# TODO: Implement populate_from and dump_to functions for Arrow.jl
+
+function populate_from_arrow!()
+    @error "Not yet implemented."
+    return
+end
+
+function dump_to_arrow!()
+    @error "Not yet implemented."
+    return
+end
+
 end
