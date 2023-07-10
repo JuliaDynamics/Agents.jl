28c75b0e6bd87a6d6341b3a741ca85b395c7b732
Tortar <68152031+Tortar@users.noreply.github.com>
2023-07-10 13:55:16 +0200

    revert last change

--- ext/AgentsArrow/AgentsArrow.jl
+++ ext/AgentsArrow/AgentsArrow.jl
@@ -4,9 +4,9 @@ using Agents, Arrow
 
 function Agents.writer_arrow(filename, data, append)
     if append
-        Arrow.write(filename, data; file = false)
+        Arrow.append(filename, data)
     else
-        Arrow.write(filename, data)
+        Arrow.write(filename, data; file = false)
     end
 end
 
