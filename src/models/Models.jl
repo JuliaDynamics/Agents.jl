ec728535a902e0d8db4e1fe379c474e47e57601f
Tortar <68152031+Tortar@users.noreply.github.com>
2023-07-28 16:11:51 +0200

    Update Models.jl

--- src/models/Models.jl
+++ src/models/Models.jl
@@ -2,7 +2,7 @@ export Models
 
 """
 Sub-module of the module `Agents`, which contains pre-defined
-agent based models shown the Examples section of the documentation.
+agent based models shown in the Examples section of the documentation.
 
 Models are represented by functions that initialize an ABM, and return the ABM,
 and the agent and model stepping functions.
