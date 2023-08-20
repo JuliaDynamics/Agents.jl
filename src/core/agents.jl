be3442b7e160f71681c7b7941bb51b66d0fc91bd
Tortar <68152031+Tortar@users.noreply.github.com>
2023-08-20 15:01:04 +0200

    mutate ContinuousAgent in macro when used as base type

--- src/core/agents.jl
+++ src/core/agents.jl
@@ -196,7 +196,11 @@ macro agent(new_name, base_type, super_type, extra_fields)
     # This macro was generated with the guidance of @rdeits on Discourse:
     # https://discourse.julialang.org/t/
     # metaprogramming-obtain-actual-type-from-symbol-for-field-inheritance/84912
-
+    if base_type isa Expr
+        if base_type.args[1] == :ContinuousAgent && length(base_type.args) == 2
+            base_type = Expr(base_type.head, base_type.args..., :Float64)
+        end
+    end
     # We start with a quote. All macros return a quote to be evaluated
     quote
         let
