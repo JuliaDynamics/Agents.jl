ed5af7e61f720b76e62ce2ead8b229d9e5d3c00e
Tortar <68152031+Tortar@users.noreply.github.com>
2023-08-22 02:56:55 +0200

    Update discrete.jl

--- src/spaces/discrete.jl
+++ src/spaces/discrete.jl
@@ -118,7 +118,7 @@ end
     empty_nearby_positions(pos, model::ABM{<:DiscreteSpace}, r = 1; kwargs...)
     empty_nearby_positions(agent, model::ABM{<:DiscreteSpace}, r = 1; kwargs...)
 
-Return an iterable of all empty nearby positions of the given position or the given agent.
+Return an iterable of all empty positions within radius `r` from the given position or the given agent.
 
 The value of `r` and possible keywords operate identically to [`nearby_positions`](@ref).
 """
