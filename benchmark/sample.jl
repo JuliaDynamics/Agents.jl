d74e6f91ae5dbe5c1153feee6dd7ee3ecb17d9c6
Tortar <68152031+Tortar@users.noreply.github.com>
2023-08-24 01:43:08 +0200

    Create sample.jl

---
+++ benchmark/sample.jl
@@ -0,0 +1,25 @@
+using Agents, BenchmarkTools
+
+@agent NoSpaceAgent begin end
+
+function fake_model(; nagents)
+    model = StandardABM(FakeAgent)
+    for i in 1:nagents
+        add_agent!(model)
+    end
+    return model
+end
+
+@benchmark sample!(model, 10^5) setup=(model = fake_model(; nagents = 10^2)) evals=1
+@benchmark sample!(model, 10^4) setup=(model = fake_model(; nagents = 10^2)) evals=1
+@benchmark sample!(model, 10^3) setup=(model = fake_model(; nagents = 10^2)) evals=1
+@benchmark sample!(model, 10^2) setup=(model = fake_model(; nagents = 10^2)) evals=1
+@benchmark sample!(model, 10^1) setup=(model = fake_model(; nagents = 10^2)) evals=1
+@benchmark sample!(model, 10^0) setup=(model = fake_model(; nagents = 10^2)) evals=1
+
+@benchmark sample!(model, 10^5) setup=(model = fake_model(; nagents = 10^5)) evals=1
+@benchmark sample!(model, 10^4) setup=(model = fake_model(; nagents = 10^5)) evals=1
+@benchmark sample!(model, 10^3) setup=(model = fake_model(; nagents = 10^5)) evals=1
+@benchmark sample!(model, 10^2) setup=(model = fake_model(; nagents = 10^5)) evals=1
+@benchmark sample!(model, 10^1) setup=(model = fake_model(; nagents = 10^5)) evals=1
+@benchmark sample!(model, 10^0) setup=(model = fake_model(; nagents = 10^5)) evals=1
