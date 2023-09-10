07259af40ff060ccb68d9c4b1f30f6e299a61f50
Tortar <68152031+Tortar@users.noreply.github.com>
2023-09-10 18:27:22 +0200

    new internal API for sampling

--- src/simulations/sample.jl
+++ src/simulations/sample.jl
@@ -100,3 +100,133 @@ end
 function choose_arg(x, kwargs_nt, agent)
     return deepcopy(getfield(hasproperty(kwargs_nt, x) ? kwargs_nt : agent, x))
 end
+
+#######################################################################################
+# %% sampling functions
+#######################################################################################
+
+# sample(iter, rng, [condition])
+# sample(iter, rng, [n, condition])
+
+function sample(iter, rng; alloc = false)
+    if alloc 
+        sampling_single(iter, rng)
+    else
+        resorvoir_sampling_single(iter, rng)
+    end
+end
+
+function sample(iter, rng, condition; alloc = false)
+    if alloc 
+        sampling_with_condition_single(iter, rng, condition)
+    else
+        iter_filtered = Iterators.filter(x -> condition(x), iter)
+        sampling_with_condition_single(iter_filtered, rng)
+    end
+end
+
+function sample(iter, rng, n::Int; alloc = true, iter_type = Any)
+    if alloc 
+        sampling_multi(iter, rng, n)
+    else
+        resorvoir_sampling_multi(iter, rng, n, iter_type)
+    end
+end
+
+function sample(iter, rng, n, condition; alloc = true)
+    if alloc 
+        sampling_with_condition_multi(iter, rng, n)
+    else
+        iter_filtered = Iterators.filter(x -> condition(x), iter)
+        resorvoir_sampling_multi(iter_filtered, rng, n, iter_type)
+    end
+end
+
+sampling_single(iter, rng) = rand(rng, collect(iter))
+
+function sampling_with_condition_single(iter, rng, condition)
+    population = collect(iter)
+    n = length(population)
+    @inbounds while n != 0
+        index_id = rand(rng, 1:n)
+        el = population[index_id]
+        condition(el) && return el
+        population[index_id], population[n] = population[n], population[index_id]
+        n -= 1
+    end
+    return nothing
+end
+
+# Reservoir sampling function (https://en.wikipedia.org/wiki/Reservoir_sampling)
+function resorvoir_sampling_single(iter, rng)
+    res = iterate(iter)
+    isnothing(res) && return nothing
+    w = rand(rng)
+    while true
+        choice, state = res
+        skip_counter = floor(log(rand(rng))/log(1 - w))
+        while skip_counter != 0
+            skip_res = iterate(iter, state)
+            isnothing(skip_res) && return choice
+            state = skip_res[2]
+            skip_counter -= 1
+        end
+        res = iterate(iter, state)
+        isnothing(res) && return choice
+        w *= rand(rng)
+    end
+end
+
+function sampling_multi(iter, rng, n)
+    population = collect(iter)
+    return sample(rng, population, n; replace=true)  
+end
+
+function sampling_with_condition_multi(iter, rng, condition, n)
+    population = collect(iter)
+    length(population) <= m && return filter(obs -> condition(obs), population)
+    res = Vector{eltype(population)}(undef, n)
+    n_pop = length(population)
+    i = 1
+    @inbounds while n_pop != 0
+        index_id = rand(rng, 1:n_pop)
+        el = population[index_id]
+        if condition(el)
+            res[i] = el
+            i == n && return res
+            i += 1         
+        end
+        population[index_id], population[n_pop] = population[n_pop], population[index_id]
+        n_pop -= 1
+    end
+    return res[1:i-1] 
+end
+
+function resorvoir_sampling_multi(iter, rng, n, iter_type = Any)
+    it = iterate(iter)
+    isnothing(it) && return type_iter[]
+    el, state = it
+    reservoir = Vector{iter_type}(undef, n)
+    reservoir[1] = el
+    for i in 2:n
+        it = iterate(iter, state)
+        isnothing(it) && return reservoir[1:i-1]
+        el, state = it
+        reservoir[i] = el
+    end
+    w = rand(rng)^(1/n)
+    while true
+        skip_counter = floor(log(rand(rng))/log(1 - w))
+        while skip_counter != 0
+            skip_it = iterate(iter, state)
+            isnothing(skip_it) && return reservoir
+            state = skip_it[2]
+            skip_counter -= 1
+        end
+        it = iterate(iter, state)
+        isnothing(it) && return reservoir
+        el, state = it
+        reservoir[rand(rng, 1:n)] = el 
+        w *= rand(rng)^(1/n)
+    end
+end
