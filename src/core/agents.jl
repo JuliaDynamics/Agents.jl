a92ed3403ca44883fb732b31388800f3c0c7027f
Tortar <68152031+Tortar@users.noreply.github.com>
2023-10-09 03:00:52 +0200

    Update agents.jl

--- src/core/agents.jl
+++ src/core/agents.jl
@@ -232,7 +232,7 @@ The minimal agent struct for usage with `nothing` as space (i.e., no space).
 It has the field `id::Int`, and potentially other internal fields that
 are not documented as part of the public API. See also [`@agent`](@ref).
 """
-mutable struct NoSpaceAgent
+mutable struct NoSpaceAgent <: AbstractAgent
     const id::Int
 end
 
