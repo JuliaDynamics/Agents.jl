1e42061dcfddb1b60da900007768d950a011a4f4
Tortar <68152031+Tortar@users.noreply.github.com>
2023-10-17 23:52:20 +0200

    Update predator_prey.jl

--- examples/predator_prey.jl
+++ examples/predator_prey.jl
@@ -61,6 +61,56 @@ end
     Δenergy::Float64
 end
 
+# The function `initialize_model` returns a new model containing sheep, wolves, and grass
+# using a set of pre-defined values (which can be overwritten). The environment is a two
+# dimensional grid space, which enables animals to walk in all
+# directions.
+
+function initialize_model(;
+        n_sheep = 100,
+        n_wolves = 50,
+        dims = (20, 20),
+        regrowth_time = 30,
+        Δenergy_sheep = 4,
+        Δenergy_wolf = 20,
+        sheep_reproduce = 0.04,
+        wolf_reproduce = 0.05,
+        seed = 23182,
+    )
+
+    rng = MersenneTwister(seed)
+    space = GridSpace(dims, periodic = true)
+    ## Model properties contain the grass as two arrays: whether it is fully grown
+    ## and the time to regrow. Also have static parameter `regrowth_time`.
+    ## Notice how the properties are a `NamedTuple` to ensure type stability.
+    properties = (
+        fully_grown = falses(dims),
+        countdown = zeros(Int, dims),
+        regrowth_time = regrowth_time,
+    )
+    model = StandardABM(Union{Sheep, Wolf}, space; 
+        agent_step! = sheepwolf_step!, model_step! = grass_step!,
+        properties, rng, scheduler = Schedulers.Randomly(), warn = false
+    )
+    ## Add agents
+    for _ in 1:n_sheep
+        energy = rand(abmrng(model), 1:(Δenergy_sheep*2)) - 1
+        add_agent!(Sheep, model, energy, sheep_reproduce, Δenergy_sheep)
+    end
+    for _ in 1:n_wolves
+        energy = rand(abmrng(model), 1:(Δenergy_wolf*2)) - 1
+        add_agent!(Wolf, model, energy, wolf_reproduce, Δenergy_wolf)
+    end
+    ## Add grass with random initial growth
+    for p in positions(model)
+        fully_grown = rand(abmrng(model), Bool)
+        countdown = fully_grown ? regrowth_time : rand(abmrng(model), 1:regrowth_time) - 1
+        model.countdown[p...] = countdown
+        model.fully_grown[p...] = fully_grown
+    end
+    return model
+end
+
 # ## Defining the stepping functions
 # Sheep and wolves behave similarly:
 # both lose 1 energy unit by moving to an adjacent position and both consume
@@ -137,56 +187,6 @@ function grass_step!(model)
             end
         end
     end
-end
-
-# The function `initialize_model` returns a new model containing sheep, wolves, and grass
-# using a set of pre-defined values (which can be overwritten). The environment is a two
-# dimensional grid space, which enables animals to walk in all
-# directions.
-
-function initialize_model(;
-        n_sheep = 100,
-        n_wolves = 50,
-        dims = (20, 20),
-        regrowth_time = 30,
-        Δenergy_sheep = 4,
-        Δenergy_wolf = 20,
-        sheep_reproduce = 0.04,
-        wolf_reproduce = 0.05,
-        seed = 23182,
-    )
-
-    rng = MersenneTwister(seed)
-    space = GridSpace(dims, periodic = true)
-    ## Model properties contain the grass as two arrays: whether it is fully grown
-    ## and the time to regrow. Also have static parameter `regrowth_time`.
-    ## Notice how the properties are a `NamedTuple` to ensure type stability.
-    properties = (
-        fully_grown = falses(dims),
-        countdown = zeros(Int, dims),
-        regrowth_time = regrowth_time,
-    )
-    model = StandardABM(Union{Sheep, Wolf}, space; 
-        agent_step! = sheepwolf_step!, model_step! = grass_step!,
-        properties, rng, scheduler = Schedulers.Randomly(), warn = false
-    )
-    ## Add agents
-    for _ in 1:n_sheep
-        energy = rand(abmrng(model), 1:(Δenergy_sheep*2)) - 1
-        add_agent!(Sheep, model, energy, sheep_reproduce, Δenergy_sheep)
-    end
-    for _ in 1:n_wolves
-        energy = rand(abmrng(model), 1:(Δenergy_wolf*2)) - 1
-        add_agent!(Wolf, model, energy, wolf_reproduce, Δenergy_wolf)
-    end
-    ## Add grass with random initial growth
-    for p in positions(model)
-        fully_grown = rand(abmrng(model), Bool)
-        countdown = fully_grown ? regrowth_time : rand(abmrng(model), 1:regrowth_time) - 1
-        model.countdown[p...] = countdown
-        model.fully_grown[p...] = fully_grown
-    end
-    return model
 end
 
 sheepwolfgrass = initialize_model()
