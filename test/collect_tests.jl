e46864ed4e3e6bbb0a74eb98889cf2d845748295
Tortar <68152031+Tortar@users.noreply.github.com>
2023-10-20 23:07:59 +0200

    Update collect_tests.jl

--- test/collect_tests.jl
+++ test/collect_tests.jl
@@ -229,41 +229,40 @@ end
                 adata = [(:weight, mean)],
                 writing_interval = 3
             )
-
-            # IO should be released manually to be able to remove the .arrow files for Windows 
-            # due to some underlying problems in Arrow.jl
-            if !(Sys.iswindows())
-                adata_saved = DataFrame(Arrow.Table("adata.arrow"))
-                @test size(adata_saved) == (11, 2)
-                @test propertynames(adata_saved) == [:step, :mean_weight]
-    
-                mdata_saved = DataFrame(Arrow.Table("mdata.arrow"))
-                @test size(mdata_saved) == (6, 3)
-                @test propertynames(mdata_saved) == [:step, :flag, :year]
-    
-                @test size(vcat(DataFrame.(Arrow.Stream("adata.arrow"))...)) == (11, 2)
-                @test size(vcat(DataFrame.(Arrow.Stream("mdata.arrow"))...)) == (6, 3)
-            else
-                open("adata.arrow") do io
-                    adata_saved = DataFrame(Arrow.Table(io))
-                    close(io)
-                end
-                @test size(adata_saved) == (11, 2)
-                @test propertynames(adata_saved) == [:step, :mean_weight]
-                open("mdata.arrow") do io
-                    mdata_saved = DataFrame(Arrow.Table(io))
-                    close(io)
-                end
-                @test size(mdata_saved) == (6, 3)
-                @test propertynames(mdata_saved) == [:step, :flag, :year]
-                @test size(vcat(DataFrame.(Arrow.Stream("adata.arrow"))...)) == (11, 2)
-                @test size(vcat(DataFrame.(Arrow.Stream("mdata.arrow"))...)) == (6, 3)
-                GC.gc(true)
+            # removing .arrow files after operating on them causes IO errors on Windows
+            # so to make tests on Windows work we need to remove them when a new test
+            # run occurs
+            if Sys.iswindows()
+                isfile("adata.arrow") && rm("adata.arrow")
+                isfile("mdata.arrow") && rm("mdata.arrow")
             end
-            rm("adata.arrow")
-            rm("mdata.arrow")
-            @test !isfile("adata.arrow")
-            @test !isfile("mdata.arrow")
+
+            offline_run!(model, 365 * 5;
+                when_model = each_year,
+                when = six_months,
+                backend = :arrow,
+                mdata = [:flag, :year],
+                adata = [(:weight, mean)],
+                writing_interval = 3
+            )
+
+            adata_saved = DataFrame(Arrow.Table("adata.arrow"))
+            @test size(adata_saved) == (11, 2)
+            @test propertynames(adata_saved) == [:step, :mean_weight]
+
+            mdata_saved = DataFrame(Arrow.Table("mdata.arrow"))
+            @test size(mdata_saved) == (6, 3)
+            @test propertynames(mdata_saved) == [:step, :flag, :year]
+
+            @test size(vcat(DataFrame.(Arrow.Stream("adata.arrow"))...)) == (11, 2)
+            @test size(vcat(DataFrame.(Arrow.Stream("mdata.arrow"))...)) == (6, 3)
+
+            if !(Sys.iswindows())
+                rm("adata.arrow")
+                rm("mdata.arrow")
+                @test !isfile("adata.arrow")
+                @test !isfile("mdata.arrow")
+            end
 
             # Backends
             @test_throws TypeError begin
