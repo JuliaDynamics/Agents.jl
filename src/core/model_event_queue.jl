369182b297d8bff601c44151585574a8f8df7afa
Tortar <68152031+Tortar@users.noreply.github.com>
2023-11-08 22:55:20 +0100

    remove steps

--- src/core/model_event_queue.jl
+++ src/core/model_event_queue.jl
@@ -13,10 +13,8 @@ struct EventQueueABM{
     S<:SpaceType,
     A<:AbstractAgent,
     C<:ContainerType{A},
-    G,K,F,P,W,L,R<:AbstractRNG} <: AgentBasedModel{S}
+    F,P,W,L,R<:AbstractRNG} <: AgentBasedModel{S}
     agents::C
-    agent_step::G
-    model_step::K
     space::S
     scheduler::F
     properties::P
@@ -31,13 +29,10 @@ end
 function EventQueueABM(
     ::Type{A},
     space::S = nothing;
-    agent_step!::G = dummystep,
-    model_step!::K = dummystep,
     container::Type = Dict{Int},
     scheduler::F = Schedulers.fastest,
     properties::P = nothing,
     rng::R = Random.default_rng(),
-    agents_first::Bool = true,
     warn = true,
     warn_deprecation = true,
     all_events::W,
@@ -46,9 +41,8 @@ function EventQueueABM(
     agent_validator(A, space, warn)
     C = construct_agent_container(container, A)
     agents = C()
-    return EventQueueABM{S,A,C,G,K,F,P,W,L,R}(agents, agent_step!, model_step!, space, scheduler,
-                                              properties, rng, Ref(0), agents_first, all_events, 
-                                              all_rates, PriorityQueue{Event, Float64}())
+    return EventQueueABM{S,A,C,G,K,F,P,W,L,R}(agents, space, scheduler, properties, rng, Ref(0),
+                                              all_events, all_rates, PriorityQueue{Event, Float64}())
 end
 
 abmqueue(model::EventQueueABM) = getfield(model, :event_queue)
