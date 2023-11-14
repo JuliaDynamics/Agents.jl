e9fe726245c966b6a3c98631fedc645b76c68a01
Frederik Banning <frederik.banning@ruhr-uni-bochum.de>
2023-11-14 15:26:01 +0100

    Expose add_interaction! function to users

--- src/visualizations.jl
+++ src/visualizations.jl
@@ -251,3 +251,14 @@ have been defined.
 function check_space_visualization_API end
 export check_space_visualization_API
 
+"""
+    add_interaction!(ax)
+    add_interaction!(ax, p::_ABMPlot)
+
+Adds model control buttons and parameter sliders according to the plotting parameters 
+`add_controls` (if true) and `params` (if not empty).
+Buttons and sliders are placed next to each other in a new layout position below the 
+position of `ax`.
+"""
+function add_interaction! end
+export add_interaction!
