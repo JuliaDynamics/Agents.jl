8d7eddc8f277d24eb692dc40c9bd787ed3167c48
Frederik Banning <frederik.banning@ruhr-uni-bochum.de>
2023-11-15 14:58:34 +0100

    Remove unneeded module annotation

--- ext/AgentsVisualizations/src/spaces/continuous.jl
+++ ext/AgentsVisualizations/src/spaces/continuous.jl
@@ -16,7 +16,7 @@ Plot heatmap according to given `heatarray`.
 Special method for models with `ContinuousSpace`.
 """
 function heatmap!(ax, p::_ABMPlot{<:Tuple{<:ABMObservable{<:Observable{<:ABM{<:S}}}}}
-    ) where {S<:Agents.ContinuousSpace}
+    ) where {S<:ContinuousSpace}
     heatobs = @lift(abmplot_heatobs($(p.abmobs[].model), p.heatarray[]))
     isnothing(heatobs[]) && return nothing
 
