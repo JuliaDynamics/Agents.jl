78d7002e4688361dc2c1a366af729dc3b9d5ed5a
Frederik Banning <frederik.banning@ruhr-uni-bochum.de>
2023-11-15 23:17:18 +0100

    Add nothing space plotting methods

---
+++ ext/AgentsVisualizations/src/spaces/nothing.jl
@@ -0,0 +1,23 @@
+## Required
+
+Agents.agents_space_dimensionality(model::ABM{Nothing}) = 
+    Agents.agents_space_dimensionality(abmspace(model))
+Agents.agents_space_dimensionality(space::Nothing) = 2
+
+Agents.get_axis_limits!(model::ABM{Nothing}) = nothing, nothing
+
+function Agents.agentsplot!(ax::Axis, model::ABM{Nothing}, p::_ABMPlot)
+    s = scatter!(p, p.pos)
+    s.visible[] = false
+    return p
+end
+
+## Preplots
+
+## Lifting
+
+Agents.abmplot_pos(model::ABM{Nothing}, offset, ids) = Point2f[(0.5, 0.5) for _ in ids]
+
+## Inspection
+
+Agents.ids_to_inspect(model::ABM{Nothing}, pos) = allids(model)
