1207b6dc30856f0855a83c70b2407bc82cee36d1
Frederik Banning <frederik.banning@ruhr-uni-bochum.de>
2023-11-15 13:36:56 +0100

    Expose add_interaction! function

--- src/visualizations.jl
+++ src/visualizations.jl
@@ -118,6 +118,17 @@ function get_ABMPlot_type()
   return AgentsVisualizations._ABMPlot
 end
 
+"""
+    add_interaction!(ax)
+    add_interaction!(ax, p::_ABMPlot)
+
+Adds model control buttons and parameter sliders according to the plotting parameters 
+`add_controls` (if true) and `params` (if not empty).
+Buttons and sliders are placed next to each other in a new layout position below the 
+position of `ax`.
+"""
+function add_interaction! end
+export add_interaction!
 
 """
     ABMObservable(model; adata, mdata, when) → abmobs
