c1688a01d7641a55e49e2ade59dbd92b083346d1
jacobusmmsmit <jacobusmmsmit@gmail.com>
2023-11-20 16:30:41 +0000

    Compare against nothing with === in src and test

--- test/visualization_tests.jl
+++ test/visualization_tests.jl
@@ -3,7 +3,7 @@
 @testset "agent visualizations" begin
 	include("../examples/agents_visualizations.jl")
 	# testing no error when removing files
-	@test rm("daisyworld.mp4") == nothing
+	@test rm("daisyworld.mp4") === nothing
 	include("../examples/zombies.jl")
-	@test rm("outbreak.mp4") == nothing
+	@test rm("outbreak.mp4") === nothing
 end
