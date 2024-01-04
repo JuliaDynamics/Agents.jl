bc5cba1750167e360286045a401aa9d7533a0937
Tortar <68152031+Tortar@users.noreply.github.com>
2024-01-04 02:29:29 +0100

    Update space_interaction_API.jl

--- src/core/space_interaction_API.jl
+++ src/core/space_interaction_API.jl
@@ -412,25 +412,3 @@ function sampling_with_condition_agents_single(iter, condition, model)
     end
     return nothing
 end
-
-# Reservoir sampling function (https://en.wikipedia.org/wiki/Reservoir_sampling)
-function resorvoir_sampling_single(iter, model)
-    res = iterate(iter)
-    isnothing(res) && return nothing
-    rng = abmrng(model)
-    el, state = res
-    w = rand(rng)
-    while true
-        skip_counter = ceil(Int, randexp(rng)/log(1-w))
-        while skip_counter != 0
-            skip_res = iterate(iter, state)
-            isnothing(skip_res) && return el
-            state = skip_res[2]
-            skip_counter += 1
-        end
-        res = iterate(iter, state)
-        isnothing(res) && return el
-        el, state = res
-        w *= rand(rng)
-    end
-end
