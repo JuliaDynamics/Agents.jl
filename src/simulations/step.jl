d2a1433bdef412bbcd0039b8018d18f965c33cd8
Datseris <datseris.george@gmail.com>
2024-02-18 18:06:42 +0000

    dont check id for vecabm

--- src/simulations/step.jl
+++ src/simulations/step.jl
@@ -23,7 +23,7 @@ for, starting from the model's initial time.
 
 See also [Advanced stepping](@ref).
 """
-function CommonSolve.step!(model::ABM, n::Union{Real, Function} = 1)
+function CommonSolve.step!(model::StandardABM, n::Union{Real, Function} = 1)
     agent_step! = agent_step_field(model)
     model_step! = model_step_field(model)
     t = getfield(model, :time)
@@ -31,21 +31,22 @@ function CommonSolve.step!(model::ABM, n::Union{Real, Function} = 1)
     return model
 end
 
-function step_ahead!(model::ABM, agent_step!, model_step!, n, t)
+function step_ahead!(model::StandardABM, agent_step!, model_step!, n, t)
     agents_first = getfield(model, :agents_first)
     t0 = t[]
     while until(t[], t0, n, model)
         !agents_first && model_step!(model)
         for id in schedule(model)
             # ensure we don't act on agent that doesn't exist
-            hasid(model, id) || continue
+            # (this condition can be skipped for `VecABM`)
+            model isa VecABM && (hasid(model, id) || continue)
             agent_step!(model[id], model)
         end
         agents_first && model_step!(model)
         t[] += 1
     end
 end
-function step_ahead!(model::ABM, agent_step!::typeof(dummystep), model_step!, n, t)
+function step_ahead!(model::StandardABM, agent_step!::typeof(dummystep), model_step!, n, t)
     t0 = t[]
     while until(t[], t0, n, model)
         model_step!(model)
