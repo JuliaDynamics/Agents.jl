37ef663da52e939e70aced7e8e62b03c9c9d20b0
Tortar <68152031+Tortar@users.noreply.github.com>
2024-02-21 02:22:49 +0100

    Update step.jl

--- src/simulations/step.jl
+++ src/simulations/step.jl
@@ -23,7 +23,7 @@ for, starting from the model's initial time.
 
 See also [Advanced stepping](@ref).
 """
-function CommonSolve.step!(model::ABM, n::Union{Real, Function} = 1)
+function CommonSolve.step!(model::StandardABM, n::Union{Real, Function} = 1)
     agent_step! = agent_step_field(model)
     model_step! = model_step_field(model)
     t = getfield(model, :time)
@@ -31,7 +31,7 @@ function CommonSolve.step!(model::ABM, n::Union{Real, Function} = 1)
     return model
 end
 
-function step_ahead!(model::ABM, agent_step!, model_step!, n, t)
+function step_ahead!(model::StandardABM, agent_step!, model_step!, n, t)
     agents_first = getfield(model, :agents_first)
     t0 = t[]
     while until(t[], t0, n, model)
@@ -46,7 +46,7 @@ function step_ahead!(model::ABM, agent_step!, model_step!, n, t)
         t[] += 1
     end
 end
-function step_ahead!(model::ABM, agent_step!::typeof(dummystep), model_step!, n, t)
+function step_ahead!(model::StandardABM, agent_step!::typeof(dummystep), model_step!, n, t)
     t0 = t[]
     while until(t[], t0, n, model)
         model_step!(model)
