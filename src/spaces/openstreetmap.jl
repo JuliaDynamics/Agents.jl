3dd43f18f8317f29b2b444ffb4a9b83c5a12ac6d
Simone Carlo Surace <simsurace@gmx.net>
2024-04-12 14:12:50 +0200

    Improve docstring format

--- src/spaces/openstreetmap.jl
+++ src/spaces/openstreetmap.jl
@@ -230,10 +230,12 @@ keywords are passed to
 [`LightOSM.shortest_path`](https://deloittedigitalapac.github.io/LightOSM.jl/docs/shortest_path/#LightOSM.shortest_path).
 
 Return `true` if a path to `dest` exists, and hence the route planning was successful.
-Otherwise return `false`. When `dest` is an invalid position, i.e. contains node indices
-that are not in the graph, or if the distance along the road is not between zero and the
-length of the road, return `false` as well. Specifying `return_trip = true` also requires
-the existence of a return path for a route to be planned.
+Otherwise return `false`. When `dest` is an invalid position, i.e. if it contains node
+indices that are not in the graph, or if the distance along the road is not between zero an
+the length of the road, return `false` as well. 
+
+Specifying `return_trip = true` also requires the existence of a return path for a route to
+be planned.
 """
 function Agents.plan_route!(
         agent::AbstractAgent,
