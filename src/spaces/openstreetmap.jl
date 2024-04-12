f20fa086c6b04ff0373803c3209539d87a835fdb
Simone Carlo Surace <simsurace@gmx.net>
2024-04-12 14:14:16 +0200

    Fix typo

--- src/spaces/openstreetmap.jl
+++ src/spaces/openstreetmap.jl
@@ -231,7 +231,7 @@ keywords are passed to
 
 Return `true` if a path to `dest` exists, and hence the route planning was successful.
 Otherwise return `false`. When `dest` is an invalid position, i.e. if it contains node
-indices that are not in the graph, or if the distance along the road is not between zero an
+indices that are not in the graph, or if the distance along the road is not between zero and
 the length of the road, return `false` as well. 
 
 Specifying `return_trip = true` also requires the existence of a return path for a route to
