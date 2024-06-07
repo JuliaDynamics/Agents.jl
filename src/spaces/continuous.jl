e4a6e4adeafda5cc35e61cc719daa99196b1b4e8
Tortar <68152031+Tortar@users.noreply.github.com>
2024-06-07 12:32:39 +0200

    Fix docstring of nearest_neighbor

--- src/spaces/continuous.jl
+++ src/spaces/continuous.jl
@@ -281,7 +281,7 @@ end
 # Continuous space exclusives: collisions, nearest neighbors
 #######################################################################################
 """
-    nearest_neighbor(agent, model::ABM{<:ContinuousSpace}, r; search=:exact) → nearest
+    nearest_neighbor(agent, model::ABM{<:ContinuousSpace}, r) → nearest
 
 Return the agent that has the closest distance to given `agent`.
 Return `nothing` if no agent is within distance `r`.
