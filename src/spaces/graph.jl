a2699111b2bab8b9dfb10976d17b70b95d9f2fac
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2024-09-09 16:22:45 +0200

    Update graph.jl

--- src/spaces/graph.jl
+++ src/spaces/graph.jl
@@ -65,11 +65,11 @@ end
 #######################################################################################
 random_position(model::ABM{<:GraphSpace}) = rand(abmrng(model), 1:nv(model))
 
-function remove_agent_from_space!(agent::AbstractAgent, model::ABM{<:GraphSpace})
-    agentpos = agent.pos
+function remove_agent_from_space!(a::AbstractAgent, model::ABM{<:GraphSpace})
+    agentpos = a.pos
     ids = ids_in_position(agentpos, model)
-    ai = findfirst(a -> a == agent.id, ids)
-    isnothing(ai) && error(lazy"Tried to remove $(agent) from the space, but that agent is not on the space")
+    ai = findfirst(id -> id == a.id, ids)
+    isnothing(ai) && error(lazy"Tried to remove agent with ID $(a.id) from the space, but that agent is not on the space")
     deleteat!(ids, ai)
     return agent
 end
