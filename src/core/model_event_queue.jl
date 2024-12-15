8cb9ba65d51417410737bcb309991e156f980c5f
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2024-12-15 02:54:03 +0100

    Update model_event_queue.jl

--- src/core/model_event_queue.jl
+++ src/core/model_event_queue.jl
@@ -153,12 +153,6 @@ function EventQueueABM(
         autogenerate_on_add = true,
         autogenerate_after_action = true,
     ) where {A<:AbstractAgent,S<:SpaceType,P,R<:AbstractRNG}
-    if warn
-        @warn "This model type is still experimental which means that it is subject to breaking changes in the
-        future. Also, while all the core functionalities have been implemented, this model type
-        has some more limited features than `StandardABM`: in particular, visualizations and
-        IO functionalities are incomplete." maxlog=1
-    end
     C = construct_agent_container(container, A)
     agents = C()
     events = SizedVector{length(events), Union{typeof.(events)...}}(events...)
