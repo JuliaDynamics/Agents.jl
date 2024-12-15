6ac8707aa6c168e964cbdf61d06d2353c3e66317
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2024-12-15 02:54:42 +0100

    Remove ismultiagenttype checks (#1116)

--- src/simulations/sample.jl
+++ src/simulations/sample.jl
@@ -104,11 +104,7 @@ function replicate!(agent::AbstractAgent, model; kwargs...)
 end
 
 function copy_agent(agent::A, model, id_new; kwargs...) where {A<:AbstractAgent}
-    # TODO: this first if statement should be removed as soon as @multiagent is removed
-    if ismultiagenttype(A)
-        args = ismultiagentsumtype(A) ? new_args_sum_t(agent, model; kwargs...) : new_args_t(agent, model; kwargs...)
-        newagent = variant_constructor(agent)(id_new, args...)
-    elseif is_sumtype(A)
+    if is_sumtype(A)
         args = new_args_sum_t(agent, model; kwargs...)
         newagent = A(variantof(agent)(id_new, args...))
     else
