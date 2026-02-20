153385d0edce9a031fd2ec4190d2e8b27ab523c1
Datseris <datseris.george@gmail.com>
2026-02-20 15:29:36 +0000

    resolve aqua problems

--- test/package_sanity_tests.jl
+++ test/package_sanity_tests.jl
@@ -1,6 +1,6 @@
-using Aqua
+using Aqua, Agents, Test
 
 @testset "Code quality" begin
-	Aqua.test_all(Agents, ambiguities = false, unbound_args = false)
+	Aqua.test_all(Agents, persistent_tasks = false, unbound_args = false, stale_deps = false)
 	@test Test.detect_ambiguities(Agents) == Tuple{Method, Method}[]
 end
